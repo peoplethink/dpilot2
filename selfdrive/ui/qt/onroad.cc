f5f589282f656445de7d19f7cbe70e3f9446d3f5
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-05-11 23:05:13 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1054,7 +1054,7 @@ void NvgWindow::drawSteer(QPainter &p) {
 
   QString str;
   
-  QRect rc(30, 426, 184, 130);
+  QRect rc(30, 430, 184, 130);
   p.setPen(QPen(QColor(0xff, 0xff, 0xff, 100), 10));
   p.setBrush(QColor(0, 0, 0, 100));
   p.drawRoundedRect(rc, 20, 20);
