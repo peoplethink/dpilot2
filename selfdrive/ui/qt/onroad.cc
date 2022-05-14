79ef82795bdfbea7be72442e4ca607550741456f
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-05-15 07:10:48 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1366,7 +1366,7 @@ void NvgWindow::drawCgear(QPainter &p) {
   p.setPen(Qt::NoPen);
 	
   if ((t_gear < 9) && (t_gear !=0)) { 
-    p.drawText( rc.center().x() - 43, rc.center().y() + 55, tgear);
+    p.drawText( rc.center().x(), rc.center().y(), tgear);
   } else if (t_gear == 14 ) { 
     p.setPen(QColor(201, 34, 49, 255));
     p.drawText(rc.center().x() - 43, rc.center().y() + 55, "R");
@@ -1375,7 +1375,7 @@ void NvgWindow::drawCgear(QPainter &p) {
     p.drawText(rc.center().x() - 43, rc.center().y() + 55, "P");
   } else if (shifter == 3 ) {  
     p.setPen(QColor(255, 255, 255, 255));
-    p.drawText(rc.center().x() - 43, rc.center().y() + 55, "N");
+    p.drawText(rc.center().x() - 50, rc.center().y() + 55, "N");
   }
   // 1 "P"   2 "D"  3 "N" 4 "R"
 
