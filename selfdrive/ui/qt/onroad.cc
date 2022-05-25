3a2ad7e42c1a91aceddf1e72d61155c6173ad258
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-05-26 08:35:23 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -676,7 +676,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
   //int scc_bus = car_params.getSccBus();
 
   QString infoText;
-  infoText.sprintf(" %s  AO(%.2f/%.2f) SR(%.2f) SC(%.2f) SD(%.2f) (A%.2f/B%.2f/C%.2f)  CPU온도 %.1f° 0814",
+  infoText.sprintf(" %s  AO(%.2f/%.2f) SR(%.2f) SC(%.2f) SD(%.2f) (A%.2f/B%.2f/C%.2f)  CPU온도 %.1f° ",
 		      lateral_state[lateralControlState],
                       live_params.getAngleOffsetDeg(),
                       live_params.getAngleOffsetAverageDeg(),
