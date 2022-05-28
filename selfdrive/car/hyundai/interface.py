fc46b121a2622f9adf56a001f946fa3197e4064e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-05-28 09:18:36 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -28,7 +28,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
 
     gas_max_bp = [0., 10., 20., 30., 40., 50., 70., 90., 130.]
-    gas_max_v = [1.685, 1.615, 1.24, 1.03, 0.71, 0.55, 0.365, 0.325, 0.20]
+    gas_max_v = [1.62, 1.59, 1.24, 1.03, 0.71, 0.55, 0.365, 0.325, 0.20]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
@@ -55,9 +55,9 @@ class CarInterface(CarInterfaceBase):
 
     # -------------PID
     if Params().get("LateralControlSelect", encoding='utf8') == "0":
-      ret.lateralTuning.pid.kf = 0.00005
+      ret.lateralTuning.pid.kf = 0.000055
       ret.lateralTuning.pid.kpBP = [0., 10., 30.]
-      ret.lateralTuning.pid.kpV = [0.025, 0.045, 0.068]
+      ret.lateralTuning.pid.kpV = [0.028, 0.05, 0.065]
       ret.lateralTuning.pid.kiBP = [0., 30.]
       ret.lateralTuning.pid.kiV = [0.005, 0.008]
       ret.lateralTuning.pid.kdBP = [0.]
