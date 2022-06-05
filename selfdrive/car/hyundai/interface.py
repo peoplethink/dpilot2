52b8c6291f00aeb539ec0885d9a51ce83d786d9d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-06 08:48:33 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -57,11 +57,11 @@ class CarInterface(CarInterfaceBase):
     if Params().get("LateralControlSelect", encoding='utf8') == "0":
       ret.lateralTuning.pid.kf = 0.00006
       ret.lateralTuning.pid.kpBP = [0., 10., 30.]
-      ret.lateralTuning.pid.kpV = [0.018, 0.045, 0.072]
+      ret.lateralTuning.pid.kpV = [0.02, 0.04, 0.062]
       ret.lateralTuning.pid.kiBP = [0., 30.]
-      ret.lateralTuning.pid.kiV = [0.002, 0.005]
+      ret.lateralTuning.pid.kiV = [0.005, 0.008]
       ret.lateralTuning.pid.kdBP = [0.]
-      ret.lateralTuning.pid.kdV = [1.0]
+      ret.lateralTuning.pid.kdV = [0.8]
       ret.lateralTuning.pid.newKfTuned = True
           
     # -------------INDI
@@ -101,10 +101,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.torque.kd = 0.02
       ret.lateralTuning.torque.deadzone = 0.001
 
-    ret.steerActuatorDelay = 0.3
+    ret.steerActuatorDelay = 0.15
     ret.steerRateCost = 0.35
     ret.steerLimitTimer = 2.5
-    ret.steerRatio = 15.8
+    ret.steerRatio = 15.5
 	
     # longitudinal
     ret.longitudinalTuning.kpBP = [0.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
