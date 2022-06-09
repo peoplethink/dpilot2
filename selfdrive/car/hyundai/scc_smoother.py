9f4eb12d9be576bf1858a423b0137d730e812a54
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-09 09:32:59 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -17,7 +17,7 @@ from selfdrive.road_speed_limiter import road_speed_limiter_get_max_speed, road_
   get_road_speed_limiter
 
 SYNC_MARGIN = 3.
-CREEP_SPEED = 3.
+CREEP_SPEED = 2.3
 
 # do not modify
 MIN_SET_SPEED_KPH = V_CRUISE_MIN
@@ -371,7 +371,7 @@ class SccSmoother:
 
     start_boost = interp(CS.out.vEgo, [0.0, 1.5 * CREEP_SPEED, 3 * CREEP_SPEED], [0.6, 0.6, 0.0])
     is_accelerating = interp(accel, [0.0, 0.2], [0.0, 1.0])
-    boost = start_boost * is_accelerating * 0.4
+    boost = start_boost * is_accelerating * 0.6
 
     accel += boost
     
