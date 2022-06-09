5423f68fb505f599bdf90ab0874b1b1993fcdb3d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-09 10:04:13 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -35,7 +35,7 @@ X_EGO_COST = 0.
 V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
-A_CHANGE_COST = 70. # 200.
+A_CHANGE_COST = 60. # 200.
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .5
 LIMIT_COST = 1e6
@@ -49,7 +49,7 @@ AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 50.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.
 AUTO_TR_V = [1.1, 1.1, 1.2, 1.3, 1.4]
 
 AUTO_TR_CRUISE_GAP = 4
-
+DIFF_RADAR_VISION = 1.5
 
 
 # Fewer timestamps don't hurt performance and lead to
@@ -63,7 +63,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 7.0
+STOP_DISTANCE = 6.5
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
