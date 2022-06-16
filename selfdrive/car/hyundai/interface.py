7f02a79a9c38753cc3299096a0447dd469c66e8f
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-16 16:20:22 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -23,13 +23,9 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
-
-    v_current_kph = current_speed * CV.MS_TO_KPH
-
-    gas_max_bp = [10., 20., 50., 70., 130., 150.]
-    gas_max_v = [1.55, 1.42, 0.41, 0.23, 0.15, 0.1]
-
-    return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
+    return CarControllerParams.ACCEL_MIN, interp(current_speed,
+                                                 CarControllerParams.ACCEL_MAX_BP,
+                                                 CarControllerParams.ACCEL_MAX_VALS)
 
   @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=[], disable_radar=False):  # pylint: disable=dangerous-default-value
@@ -117,8 +113,8 @@ class CarInterface(CarInterfaceBase):
 
     ret.stopAccel = -2.0
     ret.stoppingDecelRate = 0.4  # brake_travel/s while trying to stop
-    ret.vEgoStopping = 0.5
-    ret.vEgoStarting = 0.5
+    ret.vEgoStopping = 0.2
+    ret.vEgoStarting = 0.2
 
     # genesis
     if candidate == CAR.GENESIS:
