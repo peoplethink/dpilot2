48870256badbd95bf1ca6de544217ed7e5d1eef2
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-18 06:52:51 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -90,12 +90,12 @@ class CarInterface(CarInterfaceBase):
 
       ret.lateralTuning.torque.useSteeringAngle = True
       max_lat_accel = 2.5
-      ret.lateralTuning.torque.kp = 1.5 / max_lat_accel
-      ret.lateralTuning.torque.kf = 0.7 / max_lat_accel
-      ret.lateralTuning.torque.ki = 0.05 / max_lat_accel
+      ret.lateralTuning.torque.kp = 1.0 / max_lat_accel
+      ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
+      ret.lateralTuning.torque.ki = 0.1 / max_lat_accel
       ret.lateralTuning.torque.friction = 0.001
 
-      ret.lateralTuning.torque.kd = 0.5
+      ret.lateralTuning.torque.kd = 1.0
       ret.lateralTuning.torque.steeringAngleDeadzoneDeg = 0.5
 
     ret.steerActuatorDelay = 0.1
