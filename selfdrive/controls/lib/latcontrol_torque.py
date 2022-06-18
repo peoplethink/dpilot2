cfcdc9eec0c201cdbdb0572641d94ec6ecfaa407
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-18 10:09:52 +0900

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -23,14 +23,17 @@ from selfdrive.ntune import nTune
 FRICTION_THRESHOLD = 0.2
 
 
-def set_torque_tune(tune, MAX_LAT_ACCEL=2.5, FRICTION=0.01, steering_angle_deadzone_deg=0.0):
+def set_torque_tune(tune, MAX_LAT_ACCEL=3.3, FRICTION=0.002, steering_angle_deadzone_deg=0.5):
   tune.init('torque')
   tune.torque.useSteeringAngle = True
-  tune.torque.kp = 1.0 / MAX_LAT_ACCEL
+  tune.torque.kpBP = [0., 10., 30.]
+  tune.torque.kpV = [1.0 / MAX_LAT_ACCEL, 1.5 / MAX_LAT_ACCEL, 2.0 / MAX_LAT_ACCEL]
   tune.torque.kf = 1.0 / MAX_LAT_ACCEL
-  tune.torque.ki = 0.1 / MAX_LAT_ACCEL
+  tune.torque.kiBP = [0., 10., 30.]
+  tune.torque.kiV = [0.1 / MAX_LAT_ACCEL, 0.15 / MAX_LAT_ACCEL, 0.2 / MAX_LAT_ACCEL]
   tune.torque.friction = FRICTION
-  tune.torque.kd = 1.0
+  tune.torque.kdBP = [0.]
+  tune.torque.kdV = [0.6]
   tune.torque.steeringAngleDeadzoneDeg = steering_angle_deadzone_deg
 
 
