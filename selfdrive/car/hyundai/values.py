0c6ae1944c66e11ecc2201589b2c6dd26b47781d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-19 08:23:20 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -7,7 +7,7 @@ class CarControllerParams:
   ACCEL_MAX = 1.6
   ACCEL_MIN = -3.5
   # KRKeegan increase allowed PID accel for sluggish start
-  ACCEL_MAX_VALS = [1.7, ACCEL_MAX, 1.25 ,0.6, 0.45, 0.2]
+  ACCEL_MAX_VALS = [1.7, ACCEL_MAX, 1.0 ,0.4, 0.3, 0.2]
   ACCEL_MAX_BP =   [0.0, 12.5, 25.0, 37.5, 50.0, 62.5] # 0~20~40~60~80~100km
 
   
