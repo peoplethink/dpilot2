0c670116368a00a1c3b4d478d1349cc132285889
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-18 15:47:51 +0900

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -40,11 +40,9 @@ def set_torque_tune(tune, MAX_LAT_ACCEL=2.5, FRICTION=0.01, steering_angle_deadz
 class LatControlTorque(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
-    self.pid = PIDController((CP.lateralTuning.torque.kpBP, CP.lateralTuning.torque.kpV),
-                             (CP.lateralTuning.torque.kiBP, CP.lateralTuning.torque.kiV),
-                             k_f=CP.lateralTuning.torque.kf,
-                             k_d=(CP.lateralTuning.torque.kdBP, CP.lateralTuning.torque.kdV),
-                             pos_limit=self.steer_max, neg_limit=-self.steer_max)
+    self.pid = PIDController(CP.lateralTuning.torque.kp, CP.lateralTuning.torque.ki,
+                             k_d=CP.lateralTuning.torque.kd,
+                             k_f=CP.lateralTuning.torque.kf, pos_limit=self.steer_max, neg_limit=-self.steer_max)
     self.get_steer_feedforward = CI.get_steer_feedforward_function()
     self.use_steering_angle = CP.lateralTuning.torque.useSteeringAngle
     self.friction = CP.lateralTuning.torque.friction
