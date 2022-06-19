d73cd77e43b389f7362f73ff3bed1489d9d7f3c7
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-19 16:23:34 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -100,7 +100,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1
     #ret.steerRateCost = 0.4
     ret.steerLimitTimer = 2.5
-    ret.steerRatio = 16.5
+    ret.steerRatio = 16.0
 	
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
@@ -110,10 +110,10 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayLowerBound = 0.3
     ret.longitudinalActuatorDelayUpperBound = 0.3
 
-    ret.stopAccel = - 0.7
-    ret.stoppingDecelRate = 0.3  # brake_travel/s while trying to stop
-    ret.vEgoStopping = 0.4
-    ret.vEgoStarting = 0.4
+    ret.stopAccel = - 2.0
+    ret.stoppingDecelRate = 0.4  # brake_travel/s while trying to stop
+    ret.vEgoStopping = 0.2
+    ret.vEgoStarting = 0.2
 
     # genesis
     if candidate == CAR.GENESIS:
