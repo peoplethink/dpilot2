c2b74dbd7af2e26bec9e60d4abf32c27aa787490
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-06-20 07:57:21 +0900

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -243,7 +243,6 @@ void ui_update_params(UIState *s) {
   s->show_tpms = params.getBool("ShowTpmsUI");
   s->show_brake = params.getBool("ShowBrakeUI");
   s->show_lcr = params.getBool("ShowLcrUI");
-  s->scene.enable_radar_state = Params().getBool("DisplayRadarInfo");
   s->show_engrpm = params.getBool("ShowEngRPMUI");
   s->show_steer = params.getBool("ShowSteerUI");
 }
