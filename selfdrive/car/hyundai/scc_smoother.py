e72d3e6770f9b09ae973894bf8f7f135d6fc7352
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-07-02 06:31:11 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -369,7 +369,7 @@ class SccSmoother:
     #  if not lead.radar:
     #    brake_factor *= 0.975
 
-    start_boost = interp(CS.out.vEgo, [0.0, CREEP_SPEED, 2 * CREEP_SPEED], [0.3, 0.3, 0.0])
+    start_boost = interp(CS.out.vEgo, [0.0, CREEP_SPEED, 2 * CREEP_SPEED], [0.5, 0.3, 0.0])
     is_accelerating = interp(accel, [0.0, 0.2], [0.0, 1.0])
     boost = start_boost * is_accelerating
 
