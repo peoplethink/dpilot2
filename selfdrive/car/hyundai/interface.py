7fefa13e27b13d34ab4f2a2a8806e60ed046ef69
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-07-09 16:46:27 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -102,10 +102,12 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 15.6
 	
     # longitudinal 
-    ret.longitudinalTuning.kpBP = [0.]
-    ret.longitudinalTuning.kpV = [0.1]
+    ret.longitudinalTuning.kpBP = [0., 5., 20.]
+    ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
     ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-    ret.longitudinalTuning.kiV = [.3, .2, .18, .16, .1]
+    ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
+    ret.longitudinalTuning.deadzoneBP = [0., 8.05]
+    ret.longitudinalTuning.deadzoneV = [.0, .14]
     
     #ret.longitudinalTuning.deadzoneBP = [0., 4.]
     #ret.longitudinalTuning.deadzoneV = [0., 0.1]
