541e1f1d46e8d8f01fbee195e179d060cd113f71
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-07-10 12:50:10 +0900

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -10,7 +10,7 @@ LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
 LANE_CHANGE_SPEED_MIN = 55 * CV.KPH_TO_MS
-LANE_CHANGE_TIME_MAX = 5.
+LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
   LaneChangeDirection.none: {
