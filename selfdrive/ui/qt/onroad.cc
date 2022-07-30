14f50d9e8109bfb75afadc54d207dbbab17749ff
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-07-30 12:18:57 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -793,7 +793,7 @@ void NvgWindow::drawTpms(QPainter &p) {
   const int w = 58;
   const int h = 126;
   const int x = 110 + 1625;
-  const int y = height() - h - 160;
+  const int y = height() - h - 90;
 
   auto tpms = car_state.getTpms();
   const float fl = tpms.getFl();
