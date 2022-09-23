fdc574f871024cc14b052a4a862ce08730adcf2e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-09-23 22:43:59 +0900

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -72,7 +72,7 @@ class LatControlTorque(LatControl):
       lateral_accel_deadzone = curvature_deadzone * CS.vEgo ** 2
 
 
-      low_speed_factor = interp(CS.vEgo, [0, 15], [500, 0])
+      low_speed_factor = interp(CS.vEgo, [0, 8, 20], [100, 70, 60])
       setpoint = desired_lateral_accel + low_speed_factor * desired_curvature
 
       measurement = actual_lateral_accel + low_speed_factor * actual_curvature
