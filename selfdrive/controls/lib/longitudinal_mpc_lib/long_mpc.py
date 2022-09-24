0a98945517e7fe0809c228d66738113048f9cbc0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-09-24 23:19:58 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -61,7 +61,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 7.0
+STOP_DISTANCE = 5.5
 
 def get_stopped_equivalence_factor(v_lead, v_ego, tr):
   # KRKeegan this offset rapidly decreases the following distance when the lead pulls
@@ -258,12 +258,12 @@ class LongitudinalMpc:
   def get_cost_multipliers(self, v_lead0, v_lead1):
     v_ego = self.x0[1]
     v_ego_bps = [0, 10]
-    TFs = [1.2, 1.45, 1.8]
+    TFs = [1.0, 1.25, 1.45]
     # KRKeegan adjustments to costs for different TFs
     # these were calculated using the test_longitudial.py deceleration tests
-    a_change_tf = interp(self.param_tr, TFs, [.8, 1., 1.1])
-    j_ego_tf = interp(self.param_tr, TFs, [.8, 1., 1.1])
-    d_zone_tf = interp(self.param_tr, TFs, [1.3, 1., 1.])
+    a_change_tf = interp(self.param_tr, TFs, [.1, .8, 1.])
+    j_ego_tf = interp(self.param_tr, TFs, [.6, .8, 1.])
+    d_zone_tf = interp(self.param_tr, TFs, [1.6, 1.3, 1.])
     # KRKeegan adjustments to improve sluggish acceleration
     # do not apply to deceleration
     j_ego_v_ego = 1
@@ -277,8 +277,8 @@ class LongitudinalMpc:
     return (a_change, j_ego, d_zone_tf)
   
   def set_weights_for_lead_policy(self, prev_accel_constraint=True, v_lead0=0, v_lead1=0):
-    cost_mulitpliers = self.get_cost_multipliers(v_lead0, v_lead1)
     a_change_cost = A_CHANGE_COST if prev_accel_constraint else 0
+    cost_mulitpliers = self.get_cost_multipliers(v_lead0, v_lead1)
     W = np.asfortranarray(np.diag([X_EGO_OBSTACLE_COST, X_EGO_COST, V_EGO_COST,
                                    A_EGO_COST, a_change_cost * cost_mulitpliers[0],
                                    J_EGO_COST * cost_mulitpliers[1]]))
