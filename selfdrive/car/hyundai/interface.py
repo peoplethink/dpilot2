756d6dd6751a04f7025bcfdc5512be57599a9a4b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-09-24 23:12:12 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -27,8 +27,8 @@ class CarInterface(CarInterfaceBase):
    
     v_current_kph = current_speed * CV.MS_TO_KPH
 
-    gas_max_bp = [0., 10., 20., 50., 70., 130., 150.]
-    gas_max_v = [1.6, 1.45, 1.4, 0.5, 0.38, 0.18, 0.1]
+    gas_max_bp = [0.0, 20.0]
+    gas_max_v = [1.8, 1.5]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
@@ -102,24 +102,20 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 15.3
 	
     # longitudinal 
-    ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.2, 1.05, 1.0, 0.92, 0.55]
-    ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.1, 0.05]
-    #ret.longitudinalTuning.kpBP = [0., 5., 20.]
-    #ret.longitudinalTuning.kpV = [0.25, 0.5, 0.5]
-    #ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-    #ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
+    ret.longitudinalTuning.kpBP = [0., 5., 20.]
+    ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
+    ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
+    ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
     ret.longitudinalTuning.deadzoneBP = [0., 8.05]
     ret.longitudinalTuning.deadzoneV = [.0, .14]
 
-    ret.longitudinalActuatorDelayLowerBound = 0.3
-    ret.longitudinalActuatorDelayUpperBound = 0.3
+    #ret.longitudinalActuatorDelayLowerBound = 0.3
+    #ret.longitudinalActuatorDelayUpperBound = 0.3
 
-    ret.stopAccel = -0.8
-    ret.stoppingDecelRate = 0.25 # brake_travel/s while trying to stop
-    ret.vEgoStopping = 0.2
-    ret.vEgoStarting = 0.2
+    #ret.stopAccel = -0.8
+    ret.stoppingDecelRate = 0.3 # brake_travel/s while trying to stop
+    #ret.vEgoStopping = 0.2
+    #ret.vEgoStarting = 0.2
 
     # genesis
     if candidate == CAR.GENESIS:
