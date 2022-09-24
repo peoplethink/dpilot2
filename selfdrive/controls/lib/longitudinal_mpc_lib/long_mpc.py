c395f388fef21348edadb97429834066fa567e1d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-09-24 23:22:48 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -46,7 +46,7 @@ CRUISE_GAP_BP = [1., 2., 3., 4.]
 CRUISE_GAP_V = [0.9, 1.2, 1.45, 1.8]
 
 AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
-AUTO_TR_V = [1.1, 1.2, 1.3, 1.5]
+AUTO_TR_V = [1.0, 1.1, 1.25, 1.5]
 
 AUTO_TR_CRUISE_GAP = 4
 
