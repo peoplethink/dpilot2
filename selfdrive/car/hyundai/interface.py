2a0c9c727c06ce4165d6646952e3a1789c2a3912
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-09-28 20:24:53 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -109,8 +109,8 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.deadzoneBP = [0., 8.05]
     ret.longitudinalTuning.deadzoneV = [.0, .14]
 
-    ret.longitudinalActuatorDelayLowerBound = 0.5
-    ret.longitudinalActuatorDelayUpperBound = 0.5
+    #ret.longitudinalActuatorDelayLowerBound = 0.5
+    #ret.longitudinalActuatorDelayUpperBound = 0.5
 
     #ret.stopAccel = -0.8
     ret.stoppingDecelRate = 0.3 # brake_travel/s while trying to stop
