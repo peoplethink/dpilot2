8104dccd88cab193dd774c3910ef77a555acf276
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-10-15 06:35:36 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -36,7 +36,7 @@ V_EGO_COST = 0.
 A_EGO_COST = 0.
 J_EGO_COST = 5.0
 A_CHANGE_COST = 150.
-DANGER_ZONE_COST = 100.
+DANGER_ZONE_COST = 110.
 CRASH_DISTANCE = .5
 LIMIT_COST = 1e6
 ACADOS_SOLVER_TYPE = 'SQP_RTI'
@@ -63,8 +63,8 @@ T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.45
-COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 7.0
+COMFORT_BRAKE = 2.3
+STOP_DISTANCE = 6.0
 
 def get_stopped_equivalence_factor(v_lead, v_ego, tr):
   # KRKeegan this offset rapidly decreases the following distance when the lead pulls
