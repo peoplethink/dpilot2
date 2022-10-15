0e26ea49ddd32c0a183787f58424159929b588ae
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-10-15 13:41:00 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -115,7 +115,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
     ret.stopAccel = -2.0
-    ret.stoppingDecelRate = 0.35  # brake_travel/s while trying to stop
+    ret.stoppingDecelRate = 0.3 # brake_travel/s while trying to stop
     ret.vEgoStopping = 0.5
     ret.vEgoStarting = 0.5
 
