af7571f067a5d9957ebdb7c9703ca2fc7b655f83
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-12-30 06:39:07 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,7 +20,7 @@ from selfdrive.controls.lib.events import Events
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.3, 1.2, 0.5, 0.3]
+A_CRUISE_MAX_VALS = [1.8, 1.2, 0.5, 0.3]
 A_CRUISE_MAX_BP = [0., 15., 25., 40.]
 
 # Lookup table for turns
