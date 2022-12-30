cdff6e867df541499c7107669c97b3eaa34878cd
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2022-12-30 15:34:30 +0900

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -7,7 +7,8 @@ from common.params import Params
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
-LANE_CHANGE_SPEED_MIN = 55 * CV.KPH_TO_MS
+lanechangeset = float(int(Params().get("Lane_Change", encoding="utf8")))
+LANE_CHANGE_SPEED_MIN = lanechangeset * CV.KPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
