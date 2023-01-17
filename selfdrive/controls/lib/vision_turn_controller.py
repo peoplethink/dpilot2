46cc22faa10c2fcdb82e29b7ab0f298bd4cd1a96
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-17 23:11:22 +0900

    Update vision_turn_controller.py

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -24,7 +24,7 @@ _EVAL_START = 20.  # mts. Distance ahead where to start evaluating vision curvat
 _EVAL_LENGHT = 150.  # mts. Distance ahead where to stop evaluating vision curvature.
 _EVAL_RANGE = np.arange(_EVAL_START, _EVAL_LENGHT, _EVAL_STEP)
 
-_A_LAT_REG_MAX = 1.95
+_A_LAT_REG_MAX = 2.
 # Maximum lateral acceleration
 
 _NO_OVERSHOOT_TIME_HORIZON = 4.  # s. Time to use for velocity desired based on a_target when not overshooting.
@@ -32,12 +32,12 @@ _NO_OVERSHOOT_TIME_HORIZON = 4.  # s. Time to use for velocity desired based on 
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
 _ENTERING_SMOOTH_DECEL_V = [-0.2, -0.5]  # min decel value allowed on ENTERING state
-_ENTERING_SMOOTH_DECEL_BP = [1.25, 2.9]  # absolute value of lat acc ahead
+_ENTERING_SMOOTH_DECEL_BP = [1.3, 3.]  # absolute value of lat acc ahead
 
 #Lookup table for the acceleration for the TURNING state
 #depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.5, 0.0, -0.4]  # acc value
-_TURNING_ACC_BP = [1.5, 2.25, 3.0]  # absolute value of current lat acc
+_TURNING_ACC_V = [1.2, 0.94, 0.9, 0.8, -0.1]  # acc value
+_TURNING_ACC_BP = [1.5, 10, 12, 14, 16]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.5  # Confortble acceleration to regain speed while leaving a turn.
 
