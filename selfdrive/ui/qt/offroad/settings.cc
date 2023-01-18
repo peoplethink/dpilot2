1c3673cb124d1501e5c8810341b2704182d4a7f7
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-18 09:37:35 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -490,6 +490,10 @@ VIPPanel::VIPPanel(QWidget* parent) : QWidget(parent) {
                                             "방향지시등 작동시 상시조향 가능",
                                             "../assets/offroad/icon_openpilot.png",
                                             this));
+  layout->addWidget(new ParamControl("Sound_Mdpserr","MDPS 작동불능 음성",
+                                            "DH MDPS 작동 불가시 음성을 끄거나 켭니다.", 
+                                            "../assets/offroad/icon_shell.png", 
+                                            this));
   layout->addWidget(new ParamControl("HapticFeedbackWhenSpeedCamera",
                                             "NDA 카메라 과속시 핸들진동 선택",
                                             "NDA 카메라 과속시 핸들진동 선택",
