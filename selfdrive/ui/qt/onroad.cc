c47d1040b5a69e0037455a9c05d1d7b158203497
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-18 15:37:51 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -688,7 +688,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
   //int scc_bus = car_params.getSccBus();
 
   QString infoText;
-  infoText.sprintf("     %s           SR%.2f           CPU %.1f° ",
+  infoText.sprintf("    %s           SR%.2f           CPU %.1f° ",
 		      lateral_state[lateralControlState],
                       //live_params.getAngleOffsetDeg(),
                       //live_params.getAngleOffsetAverageDeg(),
@@ -803,7 +803,7 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
   }
 
   configFont(p, "Open Sans", 35, "Bold");
-  drawText(p, x, y-23, "", 200);
+  drawText(p, x, y-25, "", 200);
 
   configFont(p, "Open Sans", textSize, "Bold");
   drawTextWithColor(p, x-290, y+140, str, textColor);
@@ -812,7 +812,7 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
       // ajouatom: 현재시간표시
       QTextOption  textOpt = QTextOption(Qt::AlignLeft);
       configFont(p, "Open Sans", 37, "Bold");
-      p.drawText(QRect(1270, 700, width(), 150), QDateTime::currentDateTime().toString("MM월 dd일 (ddd)        hh:mm"), textOpt);
+      p.drawText(QRect(1200, 900, width(), 150), QDateTime::currentDateTime().toString("MM/dd ddd hh:mm"), textOpt);
       //configFont(p, "Open Sans", 60, "Bold");
       //p.drawText(QRect(270, 150, width(), 100), QDateTime::currentDateTime().toString("MM월 dd일 (ddd)"), textOpt);	
   }	
@@ -1373,7 +1373,7 @@ void NvgWindow::drawEngRpm(QPainter &p) {
   float textSize = 35;
 	
   int x = (width() + (bdr_s*2))/2 - bdr_s + 10;
-  int y = bdr_s + 290 + 670;
+  int y = bdr_s + 290 + 700;
 
   QString rpm;
 
