bd236ce764658f13c413560b98dc9dea5013b455
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-19 21:07:08 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -808,7 +808,7 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
   if(gap <= 0) {
     str = "N/A";
   }
-  else if(longControl && gap == 4) {
+  else if(longControl && gap == 1) {
     str = "AUTO";
     textColor = QColor(255, 255, 255, 250);
   }
