814dd3627ce9055e1883e9d609b7f3a1fa8e5e9a
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-19 21:06:23 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -362,18 +362,18 @@ class LongitudinalMpc:
   def update_TF(self, carstate):
     cruise_gap = int(clip(carstate.cruiseGap, 1., 4.))
     if cruise_gap == 1:
-      self.desired_TF = 1.0
-    elif cruise_gap == 2:
-      self.desired_TF = 1.45
-    elif cruise_gap == 3:
-      self.desired_TF = 1.8  
-    elif cruise_gap == 4: #auto
-      # At slow speeds more time, decrease time up to 60mph
-      # in mph ~= 5     10   15   20  25     30    35     40  45     50    55     60  65     70    75     80  85     90
-      x_vel = [0, 2.25, 4.5, 6.75, 9, 11.25, 13.5, 15.75, 18, 20.25, 22.5, 24.75, 27, 29.25, 31.5, 33.75, 36, 38.25, 40.5]
-      #y_dist = [1.25, 1.24, 1.23, 1.22, 1.21, 1.20, 1.18, 1.16, 1.13, 1.11, 1.09, 1.07, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05, 1.05]
-      y_dist = [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.92, 0.94, 0.96, 0.98, 1.01, 1.03, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10]
+      x_vel = [0, 3., 13.89, 25.8, 41.67]
+      y_dist = [1.0, 1.05, 1.1, 1.15, 1.2]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
+    elif cruise_gap == 2:
+      self.desired_TF = 1.2
+    elif cruise_gap == 3:
+      self.desired_TF = 1.45  
+    elif cruise_gap == 4:
+      self.desired_TF = 1.8
+      #x_vel = [0, 2.25, 4.5, 6.75, 9, 11.25, 13.5, 15.75, 18, 20.25, 22.5, 24.75, 27, 29.25, 31.5, 33.75, 36, 38.25, 40.5]
+      #y_dist = [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.92, 0.94, 0.96, 0.98, 1.01, 1.03, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10]
+      #self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
       
   def update(self, carstate, radarstate, v_cruise, prev_accel_constraint):
     v_ego = self.x0[1]
