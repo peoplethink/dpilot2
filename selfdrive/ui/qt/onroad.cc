5009f195a7a06bbeb1a145cdc1f457c93dc819ea
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-19 21:56:49 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1236,23 +1236,23 @@ void NvgWindow::drawGpsStatus(QPainter &p) {
   if(accuracy < 0.01f || accuracy > 20.f)
     return;
 
-  int w = 85;
-  int h = 65;
+  //int w = 85;
+  //int h = 65;
   int x = width() - w - 290;
   int y = 30;
 
-  p.setOpacity(0.9);
-  p.drawPixmap(x, y, w, h, ic_satellite);
+  //p.setOpacity(0.9);
+  //p.drawPixmap(x, y, w, h, ic_satellite);
 
   configFont(p, "Open Sans", 40, "Bold");
   p.setPen(QColor(255, 255, 255, 200));
-  p.setRenderHint(QPainter::TextAntialiasing);
+  //p.setRenderHint(QPainter::TextAntialiasing);
 
-  QRect rect = QRect(x, y + h + 10, w, 40);
-  rect.adjust(-30, 0, 30, 0);
+  //QRect rect = QRect(x, y + h + 10, w, 40);
+  //rect.adjust(-30, 0, 30, 0);
 
   QString str;
-  str.sprintf("%.1fm", accuracy);
+  str.sprintf("GPS%.1fm", accuracy);
   p.drawText(rect, Qt::AlignHCenter, str);
   p.setOpacity(1.);
 }
