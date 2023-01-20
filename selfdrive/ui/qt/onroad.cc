d8f4e714d3475735fa76fa7f99fbf95390eceeaf
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-20 18:12:01 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -698,8 +698,8 @@ void NvgWindow::drawCommunity(QPainter &p) {
       cpuTemp /= cpuList.size();
   }
 
-  //int mdps_bus = car_params.getMdpsBus();
-  scc_bus = car_params.getSccBus();
+  int mdps_bus = car_params.getMdpsBus();
+  int scc_bus = car_params.getSccBus();
 
   QString infoText;
   infoText.sprintf("    %s             SR%.2f             CPU %.1f°             BUS(SCC %d) ",
@@ -708,8 +708,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
                       //live_params.getAngleOffsetAverageDeg(),
                       controls_state.getSteerRatio(),
                       //controls_state.getSteerActuatorDelay(),
-                      //mdps_bus, 
-		      scc_bus,
+                      mdps_bus, scc_bus,
 	              cpuTemp
                       );
 
