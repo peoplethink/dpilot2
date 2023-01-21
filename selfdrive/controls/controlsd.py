09148b386366e2913d154e60e13c3241a5bb95af
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-21 22:43:02 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -295,6 +295,9 @@ class Controls:
       if (CS.leftBlindspot and direction == LaneChangeDirection.left) or \
          (CS.rightBlindspot and direction == LaneChangeDirection.right):
         self.events.add(EventName.laneChangeBlocked)
+       elif (((road_edge_stat = -1) and direction == LaneChangeDirection.left) or \
+         (((road_edge_stat = 1) and direction == LaneChangeDirection.right):
+        self.events.add(EventName.laneChangeBlocked) 
       else:
         if direction == LaneChangeDirection.left:
           self.events.add(EventName.preLaneChangeLeft) if lane_change_set_timer == 0 else \
