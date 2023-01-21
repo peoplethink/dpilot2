4474ede7091c9fcc44742856c2d410bbbce36b46
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-21 09:07:05 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -88,22 +88,22 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 15.8
 	
     # longitudinal
-    ret.longitudinalTuning.kpBP = [0., 5., 20., 30.]
-    ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7, 0.1]
-    ret.longitudinalTuning.kiBP = [0., 0.07, 5, 8, 11., 18., 20., 24., 33.]
-    ret.longitudinalTuning.kiV = [.001, .01, .1, .18, .21, .22, .23, .22, .001]
-    #ret.longitudinalTuning.kiBP = [0.,   5.6,  6.7,  8.3,  11.1,  19.4,   30.,  33., 40.]
-    #ret.longitudinalTuning.kiV = [.098, .126, .152, .164, .1826,  .1874,   .15,  .09, .01]
-    ret.longitudinalTuning.deadzoneBP = [0., 9.]
-    ret.longitudinalTuning.deadzoneV = [0., .15]
+    ret.longitudinalTuning.kpV = [0.5]
+    ret.longitudinalTuning.kiV = [0.0]
+    #ret.longitudinalTuning.kpBP = [0., 5., 20., 30.]
+    #ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7, 0.1]
+    #ret.longitudinalTuning.kiBP = [0., 0.07, 5, 8, 11., 18., 20., 24., 33.]
+    #ret.longitudinalTuning.kiV = [.001, .01, .1, .18, .21, .22, .23, .22, .001]
+    #ret.longitudinalTuning.deadzoneBP = [0., 9.]
+    #ret.longitudinalTuning.deadzoneV = [0., .15]
 
-    ret.longitudinalActuatorDelayLowerBound = 0.15
-    ret.longitudinalActuatorDelayUpperBound = 0.15
+    ret.longitudinalActuatorDelayLowerBound = 0.5
+    ret.longitudinalActuatorDelayUpperBound = 0.5
 
     ret.stopAccel = -2.0
     ret.stoppingDecelRate = float(int(Params().get("StoppingDecelRate", encoding="utf8"))) * 0.01
     ret.vEgoStopping = float(int(Params().get("SccStoppingCost", encoding="utf8"))) * 0.01
-    ret.vEgoStarting = 0.25
+    ret.vEgoStarting = 0.3
 
     # genesis
     if candidate == CAR.GENESIS:
