d34ea10aad9642100631af670d8ff211ce671cf6
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-21 23:44:04 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -341,7 +341,10 @@ class CarInterface(CarInterfaceBase):
     ret.sasBus = 1 if 688 in fingerprint[1] and 1296 not in fingerprint[1] else 0
     ret.sccBus = 0 if 1056 in fingerprint[0] else 1 if 1056 in fingerprint[1] and 1296 not in fingerprint[1] \
                                                                      else 2 if 1056 in fingerprint[2] else -1
-
+    ret.sccBus = 0
+      if Params().get_bool("SccConnectedBus2"):
+        ret.sccBus = 2
+	
     if ret.sccBus >= 0:
       ret.hasScc13 = 1290 in fingerprint[ret.sccBus]
       ret.hasScc14 = 905 in fingerprint[ret.sccBus]
