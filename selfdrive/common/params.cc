a194b4138a937df4bfe7f4f2d195c0d6a4d359c3
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-21 10:40:21 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -221,6 +221,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ApplyLongDynamicCost", PERSISTENT},
     {"StoppingDecelRate", PERSISTENT},
     {"SccStoppingCost", PERSISTENT},
+    {"SccStartingCost", PERSISTENT},
     {"Steer_SRTune", PERSISTENT},
     {"Steer_SRTune_v", PERSISTENT},
     {"Lane_Change", PERSISTENT},
