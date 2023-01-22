ce572ede8ec6d460ce84d3f3bd78728e4511626a
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-22 13:21:31 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -27,7 +27,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
 
     gas_max_bp = [0., 10., 20., 40., 70., 130., 150.]
-    gas_max_v = [1.6, 1.6, 1.32, 0.52, 0.32, 0.15, 0.1]
+    gas_max_v = [1.6, 1.6, 1.32, 0.5, 0.3, 0.15, 0.1]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
