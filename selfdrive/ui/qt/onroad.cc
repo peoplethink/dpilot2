6d5b8cf139d724518791c4348a07c44b7aabbc71
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-22 11:50:07 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -828,7 +828,7 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
       // ajouatom: 현재시간표시
       QTextOption  textOpt = QTextOption(Qt::AlignLeft);
       configFont(p, "Open Sans", 36, "Bold");
-      p.drawText(QRect(1147, 987, width(), 150), QDateTime::currentDateTime().toString("MM/dd/ddd/hh:mm"), textOpt);
+      p.drawText(QRect(1152, 987, width(), 150), QDateTime::currentDateTime().toString("MM/dd/ddd/ hh:mm"), textOpt);
       //configFont(p, "Open Sans", 60, "Bold");
       //p.drawText(QRect(270, 150, width(), 100), QDateTime::currentDateTime().toString("MM월 dd일 (ddd)"), textOpt);	
   }	
@@ -1247,8 +1247,8 @@ void NvgWindow::drawGpsStatus(QPainter &p) {
 
   int w = 100;
   int h = 62;
-  int x = width() - w - 140;
-  int y = 910;
+  int x = width() - w - 145;
+  int y = 914;
   p.setOpacity(1.0);
   p.drawPixmap(x, y, w, h, ic_satellite);
 
@@ -1396,7 +1396,7 @@ void NvgWindow::drawEngRpm(QPainter &p) {
   float eng_rpm = car_state.getEngRpm();
   float textSize = 35;
 	
-  int x = (width() + (bdr_s*2))/2 - bdr_s + 15;
+  int x = (width() + (bdr_s*2))/2 - bdr_s + 20;
   int y = bdr_s + 290 + 715;
 
   QString rpm;
