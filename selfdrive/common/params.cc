4da69dff8f498bab119c2fb851bc1b15dfac583b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-24 01:07:56 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -232,6 +232,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ShowDateTime", PERSISTENT},
     {"Sound_Mdpserr", PERSISTENT},
     {"ShowEngRPMUI", PERSISTENT},
+    {"Compass", PERSISTENT},
 };
 
 } // namespace
