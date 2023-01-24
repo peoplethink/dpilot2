9de866ff179e53d2dd90f0984b988383e39ade76
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-01-24 13:12:48 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -374,7 +374,7 @@ void OnroadHud::paintEvent(QPaintEvent *event) {
   }
   // compass
   if (compass && bearingAccuracyDeg != 180.00) {
-    drawCompass(p, rect().right() - radius / 2 - bdr_s + 5 * 2, radius / 2 + bdr_s + 520,
+    drawCompass(p, rect().right() - radius / 2 - bdr_s * 2, radius / 2 + bdr_s + 550,
                 compass_outer_img, blackColor(100), 5.0, bearingDeg);
   }
 }
