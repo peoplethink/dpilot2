41e0e6e55a6d8e94e4ceb8233c85612f554be888
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-06 15:56:34 +0900

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -85,7 +85,7 @@ def manager_init() -> None:
     ("SccStartingCost", "25"),
     ("Steer_SRTune", "0"),
     ("Steer_SRTune_v", "100"),
-    ("Lane_Change", "50"),
+    ("Lane_Change", "15"),
     ("SteerDeltaUp", "3"),       
     ("SteerDeltaDown", "7"),
     ("PathOffset", "0"),
