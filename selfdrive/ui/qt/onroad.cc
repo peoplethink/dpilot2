70c4b99b2d241b2b4ff32ed170abbc1dceee9e3d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-12 23:00:52 +0900

    1초마다 업뎃

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -683,7 +683,7 @@ void NvgWindow::showEvent(QShowEvent *event) {
   CameraViewWidget::showEvent(event);
 
   auto now = millis_since_boot();
-  if(now - last_update_params > 1000*5) {
+  if(now - last_update_params > 1000) {
     last_update_params = now;
     ui_update_params(uiState());
   }
