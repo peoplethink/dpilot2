e757ebb7efb952c8058856f955261db78e770d49
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-13 13:43:19 +0900

    Update settings.h

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -173,6 +173,21 @@ private:
   void refresh();
 };
 
+class LaneChangeSpeed : public AbstractControl {
+  Q_OBJECT
+
+public:
+  LaneChangeSpeed();
+
+private:
+  QPushButton btnplus;
+  QPushButton btnminus;
+  QLabel label;
+  Params params;
+  
+  void refresh();
+};
+
 class LaneLinesWidth : public AbstractControl {
   Q_OBJECT
 
