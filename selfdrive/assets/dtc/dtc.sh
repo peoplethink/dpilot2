7b0433a2c9b31a12bc9b7611d6a9a5c96bf6209e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-13 18:48:05 +0900

    Delete selfdrive/assets/dtc directory

--- selfdrive/assets/dtc/dtc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-#cd /data/openpilot/selfdrive/debug;pkill -f boardd
-cd /data/openpilot/selfdrive/assets/dtc;pkill -f boardd
-sleep 1s
-
-#./clear_dtc.py
-./dhmdps_clear_dtc.py
-sleep 5s
