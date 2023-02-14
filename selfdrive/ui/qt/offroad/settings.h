a4e8d22f4257685cc19755c0157a1f1715fd4f68
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-14 16:06:48 +0900

    Update settings.h

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -158,21 +158,6 @@ public:
   explicit VIPPanel(QWidget *parent = nullptr);
 };
 
-class AutoLaneChangeTimer : public AbstractControl {
-  Q_OBJECT
-
-public:
-  AutoLaneChangeTimer();
-
-private:
-  QPushButton btnplus;
-  QPushButton btnminus;
-  QLabel label;
-  Params params;
-
-  void refresh();
-};
-
 class LaneChangeSpeed : public AbstractControl {
   Q_OBJECT
 
