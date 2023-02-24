0a3538d1fe5e32752aee7b82d95e16010a29c94e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-24 09:23:01 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -97,10 +97,12 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 15.8
 	
     # longitudinal
-    #ret.longitudinalTuning.kpBP = [0., 5., 20., 30.]
-    #ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7, 0.1]
-    #ret.longitudinalTuning.kiBP = [0.,   3.1,  13.9,  19.4,   30.,  33.,  40.]
-    #ret.longitudinalTuning.kiV = [.032, .073, .16,   .176,   .01,  .005, .0005]
+    #ret.longitudinalTuning.kpBP = [0., 10., 40.]
+    #ret.longitudinalTuning.kpV = [1.2, 0.6, 0.2]
+    #ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
+    #ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
+    #ret.longitudinalTuning.deadzoneBP = [0., 40.]
+    #ret.longitudinalTuning.deadzoneV = [0., 0.02]
     ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     ret.longitudinalTuning.kpV = [1.3, 1.2, 1.0, 0.9, 0.5]
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
