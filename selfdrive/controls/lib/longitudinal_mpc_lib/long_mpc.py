acf056257e4c61cf7cea7905bc74de30c4cdac6b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-02-24 15:42:04 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -371,7 +371,7 @@ class LongitudinalMpc:
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
     elif cruise_gap == 4:
       x_vel = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
-      y_dist = [1.0, 1.1, 1.2, 1.45]
+      y_dist = [1.0, 1.1, 1.2, 1.3]
       self.desired_TF = np.interp(carstate.vEgo, x_vel, y_dist)
 
   def update(self, carstate, radarstate, v_cruise, prev_accel_constraint):
