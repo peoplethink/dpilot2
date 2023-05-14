29f52492243a6c79d07b93f03df8685fbd1ab7e0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-05-14 23:47:02 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -693,7 +693,11 @@ CommunityPanel::CommunityPanel(QWidget* parent) : QWidget(parent) {
                                             "휠스피드 센서 속도를 사용시 오프.",
                                             "../assets/offroad/icon_road.png",
                                             this));
-
+  toggles.append(new ParamControl("MadModeEnabled",
+                                            "Enable HKG MAD mode",
+                                            "Openpilot will engage when turn cruise control on",
+                                            "../assets/offroad/icon_openpilot.png",
+                                            this));
   toggles.append(new ParamControl("SccSmootherSlowOnCurves",
                                             "SCC기반 커브감속",
                                             "SCC 설정 시 곡률에 따른 속도 감속 기능을 사용",
