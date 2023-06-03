b8980a69bb52289812d59b416ef2858f002a9c46
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-06-03 10:41:09 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -92,7 +92,7 @@ void OnroadWindow::updateState(const UIState &s) {
     alerts->updateAlert(alert, bgColor);
   }
 
-  if (s.map_on_left) {
+  if (s.scene.map_on_left) {
     split->setDirection(QBoxLayout::LeftToRight);
   } else {
     split->setDirection(QBoxLayout::RightToLeft);
@@ -111,7 +111,7 @@ void OnroadWindow::mouseReleaseEvent(QMouseEvent* e) {
   QRect rc = rect();
   if(isMapVisible()) {
     UIState *s = uiState();
-    if(s.map_on_left)
+    if(s.scene.map_on_left)
       rc.setWidth(rc.width() - (topWidget(this)->width() / 2));
     else {
       rc.setWidth(rc.width() - (topWidget(this)->width() / 2));
@@ -172,7 +172,7 @@ void OnroadWindow::mousePressEvent(QMouseEvent* e) {
   QRect rc = rect();
   if(isMapVisible()) {
     UIState *s = uiState();
-    if(s.map_on_left)
+    if(s.scene.map_on_left)
       rc.setWidth(rc.width() - (topWidget(this)->width() / 2));
     else {
       rc.setWidth(rc.width() - (topWidget(this)->width() / 2));
