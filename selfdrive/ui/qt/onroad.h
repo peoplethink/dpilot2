de38610c18cfd071d65848464d017029bb620546
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-06-09 21:39:02 +0900

    Update onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -104,6 +104,7 @@ protected:
   void updateFrameMat(int w, int h) override;
   void drawLaneLines(QPainter &painter, const UIState *s);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd, bool is_radar);
+  void drawLockon(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd , int num);
   inline QColor redColor(int alpha = 200) { return QColor(201, 34, 49, alpha); }
   inline QColor blackColor(int alpha = 200) { return QColor(0, 0, 0, alpha); }
   inline QColor greenColor(int alpha = 200) { return QColor(49, 201, 34, alpha); }
