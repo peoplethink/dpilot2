96c096c731d6658f98e4973a85b7becf973e8189
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-06-20 23:39:49 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -24,15 +24,6 @@ class CarInterface(CarInterfaceBase):
     self.mad_mode_enabled = Params().get_bool('MadModeEnabled')
 
   @staticmethod
-  def get_pid_accel_limits(CP, current_speed, cruise_speed):
-    v_current_kph = current_speed * CV.MS_TO_KPH
-
-    gas_max_bp = [0., 10., 20., 40., 70., 110., 130.]
-    gas_max_v = [1.6, 1.58, 1.33, 0.59, 0.35, 0.17, 0.12]
-
-    return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
-
-  @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=[], disable_radar=False):  # pylint: disable=dangerous-default-value
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
 
