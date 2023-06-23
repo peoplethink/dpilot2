0703e3f2d1df3d0dfb909cce4abbfa3b070016a3
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-06-23 17:23:44 +0900

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -98,7 +98,7 @@ def manager_init() -> None:
     ("AutoNaviSpeedCtrlStart", "22"),
     ("AutoNaviSpeedCtrlEnd", "6"),
     ("LeadDangerFactor", "80"),
-    ("MaxAngleFrames", "87"),
+    ("MaxAngleFrames", "89"),
     ("PutPrebuilt", "0"), 
   ]
   if not PC:
