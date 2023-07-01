3e9e4aa35ff7d2681ab0a0bb03849fd3b66746e6
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-01 16:45:12 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -306,7 +306,7 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [2.0, 7.0]
-    ret.longitudinalTuning.kpV = [0.4, 0.9]
+    ret.longitudinalTuning.kpV = [0.4, 0.6]
 
     ret.longitudinalTuning.kiBP = [2.0, 7.0]
     ret.longitudinalTuning.kiV = [0.0, 0.15]
