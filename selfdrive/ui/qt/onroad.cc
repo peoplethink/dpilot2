bce683a3e8586d1196ac7671391f4636f22bcd83
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-02 23:13:59 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -994,7 +994,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
   const auto car_params = sm["carParams"].getCarParams();
   const auto live_params = sm["liveParameters"].getLiveParameters();
   const auto device_state = sm["deviceState"].getDeviceState();
-  const auto device_state = sm["deviceState"].getMemoryUsagePercent();	
+  float memory_usagepercent = sm["deviceState"].getDeviceState().getMemoryUsagePercent();	
   float distance_traveled = sm["controlsState"].getControlsState().getDistanceTraveled() / 1000;
   	
   int lateralControlState = controls_state.getLateralControlSelect();
