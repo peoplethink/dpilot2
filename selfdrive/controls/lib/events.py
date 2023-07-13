ea2553087d1adc8b0963141e7558714a2ff53e87
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-13 16:21:09 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -475,9 +475,9 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
 
   EventName.laneChangeBlocked: {
     ET.WARNING: Alert(
-      "차선 변경 차단(후측방감지)",
+      "Road Edge or 후측방 작동중",
       "",
-      AlertStatus.normal, AlertSize.none,
+      AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .1),
   },
 
