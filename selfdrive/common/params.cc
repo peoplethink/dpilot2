c429e82ab277d05c0e69e9c5d14b42317e549e84
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-14 08:27:45 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -252,8 +252,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LongitudinalActuatorDelayUpperBound", PERSISTENT},
     {"LongitudinalTuningKpV", PERSISTENT},
     {"LongitudinalTuningKiV", PERSISTENT},
-	{"MixRadarInfo", PERSISTENT},
-	{"OpkrUIBrightness", PERSISTENT},
+    {"MixRadarInfo", PERSISTENT},
+    {"OpkrUIBrightness", PERSISTENT},
     {"Compass", PERSISTENT},
 };
 
