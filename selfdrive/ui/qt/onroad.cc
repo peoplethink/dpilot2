8501e34ca83a6bbd16e24b39d53fd2e5f670e60b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-25 18:13:45 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1043,7 +1043,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
   const auto car_params = sm["carParams"].getCarParams();
   const auto live_params = sm["liveParameters"].getLiveParameters();
   const auto device_state = sm["deviceState"].getDeviceState();
-  float cpu_usagepercent = sm["deviceState"].getDeviceState().getCpuUsagePercent();	
+  int cpu_usagepercent = sm["deviceState"].getDeviceState().getCpuUsagePercent();	
   float distance_traveled = sm["controlsState"].getControlsState().getDistanceTraveled() / 1000;
   	
   int lateralControlState = controls_state.getLateralControlSelect();
@@ -1059,7 +1059,7 @@ void NvgWindow::drawCommunity(QPainter &p) {
   }
 
   auto cpu_loads = device_state.getCpuUsagePercent();
-  float cpu_usage = std::accumulate(cpu_loads.begin(), cpu_loads.end(), 0) / cpu_loads.size();
+  int cpu_usage = std::accumulate(cpu_loads.begin(), cpu_loads.end(), 0) / cpu_loads.size();
 	
   //int mdps_bus = car_params.getMdpsBus();
   int scc_bus = car_params.getSccBus();
