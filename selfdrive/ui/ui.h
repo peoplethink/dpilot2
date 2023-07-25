72775502bb1169820f07cfa76ba101c0706379f0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-07-25 18:45:51 +0900

    Update ui.h

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -115,7 +115,7 @@ typedef struct UIScene {
   float angleSteersDes;
   float cpuTempAvg;
   float distanceTraveled;
-  float cpuUsagePercent;
+  int cpuUsagePercent;
   float radarDistance;
   
   cereal::PandaState::PandaType pandaType;
