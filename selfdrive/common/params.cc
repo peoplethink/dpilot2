ee3def2feb449f4f3659afc14cdb51623215cf03
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-08-02 23:01:39 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -236,6 +236,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UnlimitedLength", PERSISTENT},
     {"AutoNaviSpeedCtrlStart", PERSISTENT},
     {"AutoNaviSpeedCtrlEnd", PERSISTENT},
+    {"MaxAngleFrames", PERSISTENT},
     {"LeadDangerFactor", PERSISTENT},
     {"CruiseMaxVals1", PERSISTENT},
     {"CruiseMaxVals2", PERSISTENT},
