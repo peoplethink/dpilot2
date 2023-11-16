a3f74a3ce0a553d4b90d21560ef2c9dcaba42333
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-11-16 16:25:43 +0900

    Update vision_turn_controller.py

--- selfdrive/controls/lib/vision_turn_controller.py
+++ selfdrive/controls/lib/vision_turn_controller.py
@@ -13,34 +13,34 @@ from selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX
 _MIN_V = 5.6  # Do not operate under 20km/h
 
 _ENTERING_PRED_LAT_ACC_TH = 1.3  # Predicted Lat Acc threshold to trigger entering turn state. //default 1.3 #1.4
-_ABORT_ENTERING_PRED_LAT_ACC_TH = 1.1  # Predicted Lat Acc threshold to abort entering state if speed drops.
+_ABORT_ENTERING_PRED_LAT_ACC_TH = 0.9  # Predicted Lat Acc threshold to abort entering state if speed drops.
 
-_TURNING_LAT_ACC_TH = 1.6  # Lat Acc threshold to trigger turning turn state. //default 1.6 : not working, =< 1.4 : working
+_TURNING_LAT_ACC_TH = 0.8  # Lat Acc threshold to trigger turning turn state. //default 1.6 : not working, =< 1.4 : working
 
-_LEAVING_LAT_ACC_TH = 1.3  # Lat Acc threshold to trigger leaving turn state. //default 1.3 : too long
-_FINISH_LAT_ACC_TH = 1.1  # Lat Acc threshold to trigger end of turn cycle.
+_LEAVING_LAT_ACC_TH = 0.7  # Lat Acc threshold to trigger leaving turn state. //default 1.3 : too long
+_FINISH_LAT_ACC_TH = 0.6  # Lat Acc threshold to trigger end of turn cycle.
 
 _EVAL_STEP = 5.  # mts. Resolution of the curvature evaluation.
 _EVAL_START = 20.  # mts. Distance ahead where to start evaluating vision curvature.
 _EVAL_LENGHT = 150.  # mts. Distance ahead where to stop evaluating vision curvature.
 _EVAL_RANGE = np.arange(_EVAL_START, _EVAL_LENGHT, _EVAL_STEP)
 
-_A_LAT_REG_MAX = 2.
+_A_LAT_REG_MAX = 1.7
 # Maximum lateral acceleration
 
 _NO_OVERSHOOT_TIME_HORIZON = 4.  # s. Time to use for velocity desired based on a_target when not overshooting.
 
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
-_ENTERING_SMOOTH_DECEL_V = [-0.2, -1.]  # min decel value allowed on ENTERING state
-_ENTERING_SMOOTH_DECEL_BP = [1.3, 3.]  # absolute value of lat acc ahead
+_ENTERING_SMOOTH_DECEL_V = [0.0, -0.2, -1.0]  # min decel value allowed on ENTERING state
+_ENTERING_SMOOTH_DECEL_BP = [1.25, 1.475, 2.4]  # absolute value of lat acc ahead
 
 #Lookup table for the acceleration for the TURNING state
 #depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.5, 0., -0.4]  # acc value
-_TURNING_ACC_BP = [1.5, 2.3, 3.]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.6, 0., -1.0]  # acc value
+_TURNING_ACC_BP = [0.8, 1.75, 2.4]  # absolute value of current lat acc
 
-_LEAVING_ACC = 0.5  # Confortble acceleration to regain speed while leaving a turn.
+_LEAVING_ACC = 0.7  # Confortble acceleration to regain speed while leaving a turn.
 
 _MIN_LANE_PROB = 0.6  # Minimum lanes probability to allow curvature prediction based on lanes.
 
