e3225ac9543c798cbde658227542838de7bf5362
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-11-23 15:34:50 +0900

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -34,7 +34,7 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
   else:
     if long_control_state in (LongCtrlState.off, LongCtrlState.pid):
       long_control_state = LongCtrlState.pid
-      if stopping_condition and a_target_now > -0.8:
+      if stopping_condition and a_target_now > -1.0:
         long_control_state = LongCtrlState.stopping
 
     elif long_control_state == LongCtrlState.stopping:
