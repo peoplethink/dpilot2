89989ffeb36b2e33dc17ac24b8c5e8544997e853
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-11-23 15:23:43 +0900

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -234,7 +234,7 @@ def radard_thread(sm=None, pm=None, can_sock=None):
       }
     pm.send('liveTracks', dat)
 
-    rk.monitor_time()
+    rk.keep_time()
 
 
 def main(sm=None, pm=None, can_sock=None):
