6c527ce894fbbe2a01f4d2e41fac2f4a425e6cce
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-11-25 12:57:02 +0900

    Update safety_hyundai.h

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -2,7 +2,7 @@
 
 const SteeringLimits HYUNDAI_STEERING_LIMITS = {
   .max_steer = 408,
-  .max_rt_delta = 120,
+  .max_rt_delta = 300,
   .max_rt_interval = 250000,
   .max_rate_up = 3,
   .max_rate_down = 7,
@@ -19,7 +19,7 @@ const SteeringLimits HYUNDAI_STEERING_LIMITS = {
 };
 
 const int HYUNDAI_MAX_ACCEL = 200;  // 1/100 m/s2
-const int HYUNDAI_MIN_ACCEL = -350; // 1/100 m/s2
+const int HYUNDAI_MIN_ACCEL = -370; // 1/100 m/s2
 
 const CanMsg HYUNDAI_TX_MSGS[] = {
   {832, 0, 8},  // LKAS11 Bus 0
