b0c3eccb8d55f95cb40e89cac9616ccc247e7c2c
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-12-20 17:55:58 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -83,7 +83,7 @@ class CarInterface(CarInterfaceBase):
 
 
     ret.steerActuatorDelay = 0.1
-    ret.steerLimitTimer = 0.8
+    ret.steerLimitTimer = 0.4
     ret.steerRatio = 15.3
 
     params = Params()
