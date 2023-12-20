5d67d9f41d90fab15f9f9e729da491175c33453b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-12-20 14:19:36 +0900

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -2,11 +2,8 @@ import math
 
 from cereal import log
 from common.numpy_fast import interp
-from common.filter_simple import FirstOrderFilter
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
 from selfdrive.controls.lib.pid import PIDController
-from selfdrive.controls.lib.drive_helpers import CONTROL_N, apply_deadzone
-from selfdrive.controls.lib.latcontrol import LatControl
 from selfdrive.controls.lib.vehicle_model import ACCELERATION_DUE_TO_GRAVITY
 from common.params import Params
 
@@ -53,14 +50,15 @@ class LatControlTorque(LatControl):
     if self.paramsCount > 30:
       self.paramsCount = 0
     elif self.paramsCount == 10:
-      self.lateralTorqueCustom = Params().get_int("LateralTorqueCustom")
-      self.lateralTorqueAccelFactor = float(Params().get_int("LateralTorqueAccelFactor"))*0.001
-      self.lateralTorqueFriction = float(Params().get_int("LateralTorqueFriction"))*0.001
+      self.lateralTorqueCustom = int(Params().get("LateralTorqueCustom", encoding="utf8"))
+      self.lateralTorqueAccelFactor = float(int(Params().get("LateralTorqueAccelFactor", encoding="utf8")))*0.001
+      self.lateralTorqueFriction = float(int(Params().get("LateralTorqueFriction", encoding="utf8")))*0.001
       if self.lateralTorqueCustom > 0:
         self.torque_params.latAccelFactor = self.lateralTorqueAccelFactor
         self.torque_params.friction = self.lateralTorqueFriction
         
   def update(self, active, CS, VM, params, last_actuators, steer_limited, desired_curvature, desired_curvature_rate, llk):
+    self.update_params()
     pid_log = log.ControlsState.LateralTorqueState.new_message()
 
     if CS.vEgo < MIN_STEER_SPEED or not active:
