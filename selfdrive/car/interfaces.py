018c697fc15cf0c366c8631aff71c276736d8967
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2023-12-23 08:23:35 +0900

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, Tuple, List, Callable
 from cereal import car
 from common.basedir import BASEDIR
 from common.conversions import Conversions as CV
-from common.kalman.simple_kalman import KF1D, get_kalman_gain
+from common.simple_kalman import KF1D, get_kalman_gain
 from common.numpy_fast import interp
 from common.realtime import DT_CTRL
 from selfdrive.car import gen_empty_fingerprint
@@ -306,7 +306,7 @@ class CarStateBase(ABC):
 
   def update_speed_kf(self, v_ego_raw):
     if abs(v_ego_raw - self.v_ego_kf.x[0][0]) > 2.0:  # Prevent large accelerations when car starts at non zero speed
-      self.v_ego_kf.x = [[v_ego_raw], [0.0]]
+      self.v_ego_kf.set_x([[v_ego_raw], [0.0]])
 
     v_ego_x = self.v_ego_kf.update(v_ego_raw)
     return float(v_ego_x[0]), float(v_ego_x[1])
