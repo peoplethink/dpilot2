68c29478ea3899a17219b88d6b2f798161cf7d65
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-03-09 12:41:05 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -306,12 +306,10 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = True
     ret.startingState = False
-    ret.vEgoStarting = 0.2
-    ret.vEgoStopping = 0.2
+    ret.vEgoStarting = 0.25
+    ret.vEgoStopping = 0.25
     ret.startAccel = 1.5
     ret.stoppingDecelRate = 0.2
-    ret.longitudinalActuatorDelayLowerBound = 0.5
-    ret.longitudinalActuatorDelayUpperBound = 0.5
 	  
     ret.enableBsm = 0x58b in fingerprint[0]
     ret.enableAutoHold = 1151 in fingerprint[0]
