c02586c79d6e408d43c0231fbbd9027147a5bae0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-05-27 15:54:51 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -798,8 +798,8 @@ TUNINGPanel::TUNINGPanel(QWidget* parent) : QWidget(parent) {
     toggleLayout->addWidget(new CValueControl("MaxAngleFrames", "MaxAngleFrames(89)", "89:기본, lkas fault 발생시 87:사용", "../assets/offroad/icon_road.png", 80, 100, 1));
     toggleLayout->addWidget(horizontal_line());
     toggleLayout->addWidget(new CValueControl("LateralTorqueCustom", "TorqueCustom(1)", "0:Live, 1:Custom", "../assets/offroad/icon_road.png", 0, 1, 1));
-    toggleLayout->addWidget(new CValueControl("LateralTorqueAccelFactor", "TorqueAccelFactor(2200)", "", "../assets/offroad/icon_road.png", 1000, 4000, 10));
-    toggleLayout->addWidget(new CValueControl("LateralTorqueFriction", "TorqueFriction(60)", "", "../assets/offroad/icon_road.png", 0, 1000, 10));
+    toggleLayout->addWidget(new CValueControl("LateralTorqueAccelFactor", "TorqueAccelFactor(2880)", "", "../assets/offroad/icon_road.png", 1000, 4000, 10));
+    toggleLayout->addWidget(new CValueControl("LateralTorqueFriction", "TorqueFriction(98)", "", "../assets/offroad/icon_road.png", 0, 1000, 10));
     toggleLayout->addWidget(horizontal_line());
     toggleLayout->addWidget(new CValueControl("PathOffset", "PathOffset", "(-)Left, (+)Right", "../assets/offroad/icon_road.png", -200, 200, 1));
     toggleLayout->addWidget(new LaneChangeSpeed());
