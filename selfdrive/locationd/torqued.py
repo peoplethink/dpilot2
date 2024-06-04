c73679911bac8e93224b741477324208170e56a9
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-04 17:48:57 +0900

    Update torqued.py

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -63,7 +63,8 @@ class TorqueEstimator(ParameterEstimator):
     self.offline_friction = 0.0
     self.offline_latAccelFactor = 0.0
     self.resets = 0.0
-    self.use_params = CP.carName in ALLOWED_CARS and CP.lateralTuning.which() == 'torque'
+    self.use_params = False
+    #self.use_params = CP.carName in ALLOWED_CARS and CP.lateralTuning.which() == 'torque'
 
     if CP.lateralTuning.which() == 'torque':
       self.offline_friction = CP.lateralTuning.torque.friction
