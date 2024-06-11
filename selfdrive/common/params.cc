4b190bf9857ab529d61907fac2d7e2e640fe6174
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-12 07:00:17 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -255,6 +255,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ALeadTau", PERSISTENT},
     {"ALeadTauStart", PERSISTENT},
     {"Compass", PERSISTENT},
+    {"StopDistance", PERSISTENT},
 };
 
 } // namespace
