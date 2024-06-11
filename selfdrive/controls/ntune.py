128dc62c269c712f9bb70957f31040b141aeb292
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-11 15:28:32 +0900

    Update ntune.py

--- selfdrive/controls/ntune.py
+++ selfdrive/controls/ntune.py
@@ -222,6 +222,15 @@ class nTune():
     if self.checkValue("longDisableRadar", 0., 1., 0.):
       updated = True
 
+    if self.checkValue("sccGasFactor", 0.5, 1.5, 1.0):
+      updated = True
+
+    if self.checkValue("sccBrakeFactor", 0.5, 1.5, 1.0):
+      updated = True
+
+    if self.checkValue("sccCurvatureFactor", 0.5, 1.5, 0.98):
+      updated = True
+      
     return updated
 
   def updateIndi(self):
