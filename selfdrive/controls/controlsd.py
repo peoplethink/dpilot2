bf4c4249ffeec1a3f47b591a49548292883d9fc7
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-11 15:27:47 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -842,14 +842,6 @@ class Controls:
     controlsState.sccStockCamStatus = self.sccStockCamStatus
 
     controlsState.steerRatio = self.VM.sR
-    controlsState.steerRateCost = ntune_common_get('steerRateCost')
-    controlsState.steerActuatorDelay = float(Decimal(Params().get("SteerActuatorDelayAdj", encoding="utf8")) * Decimal('0.01'))
-
-    controlsState.sccGasFactor = ntune_scc_get('sccGasFactor')
-    controlsState.sccBrakeFactor = ntune_scc_get('sccBrakeFactor')
-    controlsState.sccCurvatureFactor = ntune_scc_get('sccCurvatureFactor')
-    controlsState.totalCameraOffset = totalCameraOffset
-    
     controlsState.lateralControlSelect = int(self.lateral_control_select)
     
     if self.joystick_mode:
