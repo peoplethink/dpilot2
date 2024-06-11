661b0646b0dfe2b7df77b4957e504191cbcb4c70
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-11 16:25:24 +0900

    Update latcontrol_lqr.py

--- selfdrive/controls/lib/latcontrol_lqr.py
+++ selfdrive/controls/lib/latcontrol_lqr.py
@@ -6,7 +6,7 @@ from common.realtime import DT_CTRL
 from cereal import log
 from selfdrive.controls.lib.latcontrol import LatControl
 from common.conversions import Conversions as CV
-from selfdrive.controls.ntune import ntune
+from selfdrive.controls.ntune import nTune
 
 TORQUE_SCALE_BP = [0., 30., 80., 100., 130.]
 TORQUE_SCALE_V = [0.2, 0.35, 0.63, 0.67, 0.7]
