425e2adfdb8f1a3a71db4216d4af6db9065ef918
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-11 15:58:36 +0900

    Update ntune.py

--- selfdrive/controls/ntune.py
+++ selfdrive/controls/ntune.py
@@ -171,7 +171,7 @@ class nTune():
     if self.checkValue("steerActuatorDelay", 0., 0.8, 0.3):
       updated = True
 
-    if self.checkValue("pathFactor", 0.9, 1.1, 0.96):
+    if self.checkValue("cameraOffset", -1.0, 1.0, 0.06):
       updated = True
 
     return updated
