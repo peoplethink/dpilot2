6d0471eccabfe9a155e9c4b8d2283ef9d4bfcd50
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-23 07:23:56 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -312,7 +312,7 @@ class SccSmoother:
         if 0. < d < -lead.vRel * (9. + 3.) * 2. and lead.vRel < -1.:
           t = d / lead.vRel
           accel = -(lead.vRel / t) * self.speed_conv_to_clu
-          accel *= 1.0
+          accel *= 0.8
 
           if accel < 0.:
             target_speed = clu11_speed + accel
