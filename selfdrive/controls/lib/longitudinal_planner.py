59e2e121ddf2b417977fdabf2b619c8e58524e5a
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-06-23 07:01:19 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -20,8 +20,8 @@ from selfdrive.controls.lib.events import Events
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN_VALS = [-0.5, -0.5, -0.2, -0.3, -0.4, -1.2] # mimick stock, slightly release brakes when stopping
 A_CRUISE_MIN_BP =   [0., 0.3, 0.35, 3., 6., 20.]
-A_CRUISE_MAX_VALS = [2.0, 1.4, 0.5, 0.2, 0.15]
-A_CRUISE_MAX_BP = [0., 40 * CV.KPH_TO_MS, 60 * CV.KPH_TO_MS, 80 * CV.KPH_TO_MS, 110 * CV.KPH_TO_MS, 140 * CV.KPH_TO_MS]
+A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
+A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [2.5, 3.2]
