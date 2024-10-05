c0597eb4a12db42d039c605ac6a2455cb1cd44fe
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-10-05 14:00:18 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -300,22 +300,16 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = car.CarParams.SteerControlType.torque
 
     # longitudinal
-    #ret.longitudinalTuning.kpV = [0.5]
-    ret.longitudinalTuning.kiV = [0.0]
-
     ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.1, 1.0, 0.9, 0.6, 0.3]
-
-    #ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 30.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    #ret.longitudinalTuning.kpV = [1.0, 0.9, 0.8, 0.4, 0.2]
-    #ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
-    #ret.longitudinalTuning.kiV = [0.1, 0.05]
+    ret.longitudinalTuning.kpV = [1.2, 1.05, 1.0, 0.92, 0.55]
+    ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kiV = [0.08, 0.05]
 
     ret.stoppingControl = True
-    ret.startingState = True
-    ret.vEgoStarting = ntune_scc_get('vEgoStarting')
-    ret.vEgoStopping = ntune_scc_get('vEgoStopping')
-    ret.stoppingDecelRate = ntune_scc_get('stoppingDecelRate')
+    ret.startingState = False
+    ret.vEgoStarting = 0.3 #ntune_scc_get('vEgoStarting')
+    ret.vEgoStopping = 0.3 #ntune_scc_get('vEgoStopping')
+    ret.stoppingDecelRate = 0.2 # ntune_scc_get('stoppingDecelRate')
     ret.startAccel = ntune_scc_get('startAccel')
     ret.stopAccel = ntune_scc_get('stopAccel')  
     ret.longitudinalActuatorDelayLowerBound = 0.3
