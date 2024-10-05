14b6868f64b8d445d58a4a71721d48f1b3e4ea1e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-10-05 10:29:21 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -252,6 +252,31 @@ def no_gps_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_
     AlertStatus.normal, AlertSize.mid,
     Priority.LOWER, VisualAlert.none, AudibleAlert.none, .2, creation_delay=300.)
 
+def torque_nn_load_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
+  model_name = CP.lateralTuning.torque.nnModelName
+  fuzzy = CP.lateralTuning.torque.nnModelFuzzyMatch
+  if model_name == "":
+    return Alert(
+      "NN torque controller not loaded",
+      "go donate logs to twilsonco to get loaded!",
+      AlertStatus.userPrompt, AlertSize.mid,
+      Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
+  else:
+    if 'b\'' in model_name:
+      _car, eps = model_name.split('b\'')
+      eps = 'b\'' + eps
+      return Alert(
+        f"NN torque ({fuzzy = }): {_car}",
+        f"eps: {eps}",
+        AlertStatus.userPrompt, AlertSize.mid,
+        Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
+    else:
+      return Alert(
+        f"NN torque controller loaded ({fuzzy = })",
+        model_name,
+        AlertStatus.userPrompt, AlertSize.mid,
+        Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
+
 def low_memory_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
   return NormalPermanentAlert("Low Memory", f"{sm['deviceState'].memoryUsagePercent}% used")
 
@@ -913,6 +938,10 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .2),
   },
 
+   EventName.torqueNNLoad: {
+    ET.PERMANENT: torque_nn_load_alert,
+  },
+  
   EventName.slowingDownSpeed: {
     ET.PERMANENT: Alert("과속카메라 감지 : 감속중","", AlertStatus.normal, AlertSize.small,
       Priority.MID, VisualAlert.none, AudibleAlert.none, .1),
