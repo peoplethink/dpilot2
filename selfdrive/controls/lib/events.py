9acbb693fcc8975adc34d6122ca08964b734bc27
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2024-10-05 11:11:10 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -1,10 +1,11 @@
 import os
 from enum import IntEnum
 from typing import Dict, Union, Callable, List, Optional
-
+from common.params import Params
 from cereal import log, car
 import cereal.messaging as messaging
 from common.conversions import Conversions as CV
+
 from common.realtime import DT_CTRL
 from selfdrive.locationd.calibrationd import MIN_SPEED_FILTER
 from selfdrive.version import get_short_branch
@@ -253,29 +254,19 @@ def no_gps_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_
     Priority.LOWER, VisualAlert.none, AudibleAlert.none, .2, creation_delay=300.)
 
 def torque_nn_load_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
-  model_name = CP.lateralTuning.torque.nnModelName
-  fuzzy = CP.lateralTuning.torque.nnModelFuzzyMatch
+  model_name = Params().get("NNFFModelName", encoding='utf-8')
   if model_name == "":
     return Alert(
-      "NN torque controller not loaded",
-      "go donate logs to twilsonco to get loaded!",
+      "NNFF Torque Controller not available",
+      "Donate logs to Twilsonco to get it added!",
       AlertStatus.userPrompt, AlertSize.mid,
       Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
   else:
-    if 'b\'' in model_name:
-      _car, eps = model_name.split('b\'')
-      eps = 'b\'' + eps
-      return Alert(
-        f"NN torque ({fuzzy = }): {_car}",
-        f"eps: {eps}",
-        AlertStatus.userPrompt, AlertSize.mid,
-        Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
-    else:
-      return Alert(
-        f"NN torque controller loaded ({fuzzy = })",
-        model_name,
-        AlertStatus.userPrompt, AlertSize.mid,
-        Priority.LOW, VisualAlert.none, AudibleAlert.prompt, 6.0)
+    return Alert(
+      "NNFF Torque Controller loaded",
+      model_name,
+      AlertStatus.userPrompt, AlertSize.mid,
+      Priority.LOW, VisualAlert.none, AudibleAlert.engage, 5.0)
 
 def low_memory_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int) -> Alert:
   return NormalPermanentAlert("Low Memory", f"{sm['deviceState'].memoryUsagePercent}% used")
