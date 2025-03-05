39c3003cb5380ef3f68f2ab28b5a54722e04f6c2
durango-op <71580184+durango-op@users.noreply.github.com>
2025-03-05 09:15:14 +0900

    Update ntune.py

--- selfdrive/controls/ntune.py
+++ selfdrive/controls/ntune.py
@@ -214,6 +214,9 @@ class nTune():
     if self.checkValue("longStartingFactor", 0.7, 1.5, 1.0):
       updated = True
 
+    if self.checkValue("longitudinalActuatorDelay", 0.1, 1.0, 0.5):
+      updated = True
+      
     if self.checkValue("aTargetFactor", 0.7, 1.5, 1.0):
       updated = True
       
@@ -238,10 +241,10 @@ class nTune():
     if self.checkValue("sccBrakeFactor", 0.5, 1.5, 1.0):
       updated = True
 
-    if self.checkValue("longitudinalTuningkf", 0.0, 2.0, 1.0):
-      updated = True
-      
     if self.checkValue("sccCurvatureFactor", 0.5, 1.5, 0.98):
+      updated = True
+
+    if self.checkValue("longitudinalTuningkf", 0.0, 2.0, 1.0):
       updated = True
 
     if self.checkValue("stoppingDecelRate", 0.0, 1.0, 0.3):
@@ -253,7 +256,7 @@ class nTune():
     if self.checkValue("vEgoStarting", 0.0, 1.0, 0.3):
       updated = True
 
-    if self.checkValue("stopAccel", 0.0, 5.0, 2.0):
+    if self.checkValue("stopAccel", -5.0, 0.0, -2.0):
       updated = True
 
     if self.checkValue("startAccel", 0.0, 5.0, 1.5):
