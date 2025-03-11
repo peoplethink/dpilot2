2279ab4adc773dd3391bb80949d811c1be813bd3
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-03-11 09:25:26 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -87,12 +87,14 @@ class Planner:
       v = np.interp(T_IDXS_MPC, T_IDXS, model_msg.velocity.x) - model_error
       a = np.interp(T_IDXS_MPC, T_IDXS, model_msg.acceleration.x) 
       j = np.zeros(len(T_IDXS_MPC))
+      y = np.interp(T_IDXS_MPC, T_IDXS, model_msg.position.y)
     else:
       x = np.zeros(len(T_IDXS_MPC))
       v = np.zeros(len(T_IDXS_MPC))
       a = np.zeros(len(T_IDXS_MPC))
       j = np.zeros(len(T_IDXS_MPC)) 
-    return x, v, a, j
+      y = np.zeros(len(T_IDXS_MPC))
+    return x, v, a, j, y
 
   def update(self, sm, read=True):
     if self.param_read_counter % 50 == 0 and read:
@@ -152,8 +154,8 @@ class Planner:
     self.mpc.set_weights(prev_accel_constraint)
     self.mpc.set_accel_limits(accel_limits_turns[0], accel_limits_turns[1])
     self.mpc.set_cur_state(self.v_desired_filter.x, self.a_desired)
-    x, v, a, j = self.parse_model(sm['modelV2'], self.v_model_error)
-    self.mpc.update(sm['carState'], sm['radarState'], v_cruise, x, v, a, j)
+    x, v, a, j, y = self.parse_model(sm['modelV2'], self.v_model_error)
+    self.mpc.update(sm['carState'], sm['radarState'], sm['modelV2'], sm['controlsState'],  v_cruise, x, v, a, j, y)
 
     self.v_desired_trajectory = np.interp(T_IDXS[:CONTROL_N], T_IDXS_MPC, self.mpc.v_solution)
     self.a_desired_trajectory = np.interp(T_IDXS[:CONTROL_N], T_IDXS_MPC, self.mpc.a_solution)
@@ -190,6 +192,9 @@ class Planner:
     longitudinalPlan.visionCurrentLatAcc = float(self.vision_turn_controller.current_lat_acc)
     longitudinalPlan.visionMaxPredLatAcc = float(self.vision_turn_controller.max_pred_lat_acc)
     longitudinalPlan.eventsDEPRECATED = self.events.to_msg()
+    longitudinalPlan.trafficState = self.mpc.trafficState
+    longitudinalPlan.xState = self.mpc.xState
+    longitudinalPlan.xStop = float(self.mpc.stopDist)  
     
     longitudinalPlan.fcw = self.fcw
 
