4501291efabe7da2a9bc53286815978474ff312b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-03-11 12:28:56 +0900

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -316,8 +316,7 @@ def radard_thread(sm: Optional[messaging.SubMaster] = None, pm: Optional[messagi
 
   # wait for stats about the car to come in from controls
   cloudlog.info("radard is waiting for CarParams")
-  with car.CarParams.from_bytes(Params().get("CarParams", block=True)) as msg:
-    CP = msg
+  CP = car.CarParams.from_bytes(Params().get("CarParams", block=True))
   cloudlog.info("radard got CarParams")
 
   # import the radar from the fingerprint
