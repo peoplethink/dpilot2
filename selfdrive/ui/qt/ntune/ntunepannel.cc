fe1c5594873d4936af54f0bd2c9fd2b49cc1c2e0
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-03-11 15:32:25 +0900

    Update ntunepannel.cc

--- selfdrive/ui/qt/ntune/ntunepannel.cc
+++ selfdrive/ui/qt/ntune/ntunepannel.cc
@@ -14,6 +14,8 @@ QList<QList<TuneItemInfo>> nTuneMainWidget::mainItems = {
 {
     TuneItemInfo("common.json", "cameraOffset", "우측이동 - , 좌측이동 +",
                  0.6f, -2.0f, 2.0f, 0.05f, 2),
+    TuneItemInfo("common.json", "dampilngfactor", "",
+                 0.0f, -10.0f, 10.0f, 0.05f, 2),
     TuneItemInfo("common.json", "autoLaneChangeSpeed", "해당속도이상에서 자동차선변경",
                  30.0f, 10.0f, 60.0f, 5.0f, 1, "km"),
     TuneItemInfo("common.json", "steerActuatorDelay", "값이크면 조향을 좀더 미리함",
