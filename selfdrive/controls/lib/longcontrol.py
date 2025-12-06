d91e7650fcfde65bd2aca910206708bc11016850
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-06 21:37:31 +0900

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -5,7 +5,6 @@ from selfdrive.controls.lib.drive_helpers import CONTROL_N, apply_deadzone
 from selfdrive.controls.lib.pid import PIDController
 from selfdrive.modeld.constants import T_IDXS
 from common.conversions import Conversions as CV
-from selfdrive.controls.ntune import ntune_scc_get
 
 LongCtrlState = car.CarControl.Actuators.LongControlState
 
@@ -24,12 +23,11 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
                         accelerating and
                         not cruise_standstill and
                         not brake_pressed)
-                               
+
   started_condition = v_ego > CP.vEgoStarting
 
   if not active:
     long_control_state = LongCtrlState.off
-
   else:
     if long_control_state in (LongCtrlState.off, LongCtrlState.pid):
       long_control_state = LongCtrlState.pid
@@ -46,7 +44,7 @@ def long_control_state_trans(CP, active, long_control_state, v_ego, v_target,
       if stopping_condition:
         long_control_state = LongCtrlState.stopping
       elif started_condition:
-        long_control_state = LongCtrlState.pid  
+        long_control_state = LongCtrlState.pid
 
   return long_control_state
 
@@ -62,72 +60,91 @@ class LongControl:
                              derivative_period=0.5, rate=1 / DT_CTRL)
     self.v_pid = 0.0
     self.last_output_accel = 0.0
-    
+
   def reset(self, v_pid):
     """Reset PID controller and change setpoint"""
     self.pid.reset()
     self.v_pid = v_pid
-    
+
   def update(self, active, CS, long_plan, accel_limits, t_since_plan):
     """Update longitudinal control. This updates the state machine and runs a PID loop"""
+
     # Interp control trajectory
     speeds = long_plan.speeds
     a_target_now = 0.0
+    a_target = 0.0
+    v_target = 0.0
+    v_target_now = 0.0
+    v_target_1sec = 0.0
+
     if len(speeds) == CONTROL_N:
       v_target_now = interp(t_since_plan, T_IDXS[:CONTROL_N], speeds)
       a_target_now = interp(t_since_plan, T_IDXS[:CONTROL_N], long_plan.accels)
 
-      longitudinalActuatorDelay = ntune_scc_get('longitudinalActuatorDelay')
-      
-      v_target = interp(longitudinalActuatorDelay + t_since_plan, T_IDXS[:CONTROL_N], speeds)
-      a_target = 2 * (v_target - v_target_now) / longitudinalActuatorDelay - a_target_now
+      # 액추에이터 딜레이는 CP에서 세팅한 값 사용 (기본값 기준)
+      longitudinalActuatorDelay = max(0.1, float(self.CP.longitudinalActuatorDelay))
 
-      v_target_1sec = interp(longitudinalActuatorDelay + t_since_plan + 1.0, T_IDXS[:CONTROL_N], speeds)
-      
-    else:
-      v_target = 0.0
-      v_target_now = 0.0
-      v_target_1sec = 0.0
-      a_target = 0.0
+      v_target = interp(longitudinalActuatorDelay + t_since_plan,
+                        T_IDXS[:CONTROL_N], speeds)
+      # 딜레이를 고려한 목표 가속도 재계산
+      a_target = 2.0 * (v_target - v_target_now) / longitudinalActuatorDelay - a_target_now
+
+      v_target_1sec = interp(longitudinalActuatorDelay + t_since_plan + 1.0,
+                             T_IDXS[:CONTROL_N], speeds)
 
     self.pid.neg_limit = accel_limits[0]
     self.pid.pos_limit = accel_limits[1]
 
     output_accel = self.last_output_accel
-    
-    self.long_control_state = long_control_state_trans(self.CP, active, self.long_control_state, CS.vEgo,
-                                                       v_target, v_target_1sec, CS.brakePressed,
-                                                       CS.cruiseState.standstill)
+
+    # 상태 머신 업데이트
+    self.long_control_state = long_control_state_trans(
+      self.CP, active, self.long_control_state, CS.vEgo,
+      v_target, v_target_1sec, CS.brakePressed,
+      CS.cruiseState.standstill)
 
     if self.long_control_state == LongCtrlState.off:
       self.reset(CS.vEgo)
-      output_accel = 0.
+      output_accel = 0.0
 
     elif self.long_control_state == LongCtrlState.stopping:
-      if output_accel > ntune_scc_get('stopAccel'):
+      # 정지 단계: 너무 세게 브레이크 잡지 않도록 CP.stopAccel과 CP.stoppingDecelRate 사용
+      if output_accel > self.CP.stopAccel:
         output_accel = min(output_accel, 0.0)
-        output_accel -= ntune_scc_get('stoppingDecelRate') * DT_CTRL
+        output_accel -= self.CP.stoppingDecelRate * DT_CTRL
       self.reset(CS.vEgo)
 
     elif self.long_control_state == LongCtrlState.starting:
-      output_accel = ntune_scc_get('startAccel')
+      # 출발 단계: CP.startAccel 사용
+      output_accel = self.CP.startAccel
       self.reset(CS.vEgo)
-      
+
     elif self.long_control_state == LongCtrlState.pid:
       self.v_pid = v_target_now
 
-      # Toyota starts braking more when it thinks you want to stop
-      # Freeze the integrator so we don't accelerate to compensate, and don't allow positive acceleration
-      prevent_overshoot = not self.CP.stoppingControl and CS.vEgo < 1.5 and v_target_1sec < 0.7 and v_target_1sec < self.v_pid
-      deadzone = interp(CS.vEgo, self.CP.longitudinalTuning.deadzoneBP, self.CP.longitudinalTuning.deadzoneV)
+      # 정지 직전 오버슈트 방지 로직 (기존 openpilot 패턴 유지)
+      prevent_overshoot = (not self.CP.stoppingControl and
+                           CS.vEgo < 1.5 and
+                           v_target_1sec < 0.7 and
+                           v_target_1sec < self.v_pid)
+
+      deadzone = interp(CS.vEgo,
+                        self.CP.longitudinalTuning.deadzoneBP,
+                        self.CP.longitudinalTuning.deadzoneV)
       freeze_integrator = prevent_overshoot
 
       error = self.v_pid - CS.vEgo
       error_deadzone = apply_deadzone(error, deadzone)
-      output_accel = self.pid.update(error_deadzone, speed=CS.vEgo,
-                                     feedforward=a_target * ntune_scc_get('aTargetFactor'),
+
+      # 타겟 가속 feedforward가 너무 커지면 브레이크를 툭 치는 현상 생길 수 있어서
+      # 승용차 기준 편한 범위로 클립 (-2.0 ~ 2.0 m/s^2)
+      a_ff = clip(a_target, -2.0, 2.0)
+
+      output_accel = self.pid.update(error_deadzone,
+                                     speed=CS.vEgo,
+                                     feedforward=a_ff,
                                      freeze_integrator=freeze_integrator)
 
+    # 최종 출력 클립
     self.last_output_accel = clip(output_accel, accel_limits[0], accel_limits[1])
-    
     return self.last_output_accel
