111097edd10d6987190eeb111ef5457c05924445
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-13 23:48:18 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -19,26 +19,22 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 LongCtrlState = car.CarControl.Actuators.LongControlState
 min_set_speed = 30 * CV.KPH_TO_MS
 
-# EPS faults if you apply torque while the steering angle is above 90 degrees for more than 1 second
-# All slightly below EPS thresholds to avoid fault
 MAX_ANGLE = 85
 MAX_ANGLE_FRAMES = 89
 MAX_ANGLE_CONSECUTIVE_FRAMES = 2
 
-def process_hud_alert(enabled, fingerprint, hud_control):
 
+def process_hud_alert(enabled, fingerprint, hud_control):
   sys_warning = (hud_control.visualAlert in (VisualAlert.steerRequired, VisualAlert.ldw))
 
-  # initialize to no line visible
   sys_state = 1
-  if hud_control.leftLaneVisible and hud_control.rightLaneVisible or sys_warning:  # HUD alert only display when LKAS status is active
+  if hud_control.leftLaneVisible and hud_control.rightLaneVisible or sys_warning:
     sys_state = 3 if enabled or sys_warning else 4
   elif hud_control.leftLaneVisible:
     sys_state = 5
   elif hud_control.rightLaneVisible:
     sys_state = 6
 
-  # initialize to no warnings
   left_lane_warning = 0
   right_lane_warning = 0
   if hud_control.leftLaneDepart:
@@ -73,7 +69,6 @@ class CarController:
     self.turning_indicator_alert = False
 
     param = Params()
-
     self.mad_mode_enabled = param.get_bool('MadModeEnabled')
     self.ldws_opt = param.get_bool('IsLdwsCar')
     self.stock_navi_decel_enabled = param.get_bool('StockNaviDecelEnabled')
@@ -81,34 +76,36 @@ class CarController:
     self.haptic_feedback_speed_camera = param.get_bool('HapticFeedbackWhenSpeedCamera')
 
     self.scc_smoother = SccSmoother()
-    self.last_blinker_frame = 0
     self.prev_active_cam = False
     self.active_cam_timer = 0
     self.last_active_cam_frame = 0
+
     self.maxAngleFrames = MAX_ANGLE_FRAMES
     self.angle_limit_counter = 0
-     
+
+    # ===== apilot jerk 통합 =====
+    self.jerkStartLimit = 1.0
+    self.jerk_count = 0.0
+    # ===========================
+
   def update(self, CC, CS, controls):
     actuators = CC.actuators
     hud_control = CC.hudControl
-    pcm_cancel_cmd = CC.cruiseControl.cancel
 
-    # Steering Torque
     new_steer = int(round(actuators.steer * self.params.STEER_MAX))
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
 
-    # >90 degree steering fault prevention
-    self.angle_limit_counter, apply_steer_req = common_fault_avoidance(abs(CS.out.steeringAngleDeg) >= MAX_ANGLE, CC.latActive,
-                                                                       self.angle_limit_counter, self.maxAngleFrames,
-                                                                       MAX_ANGLE_CONSECUTIVE_FRAMES)
-    
-    # disable when temp fault is active, or below LKA minimum speed
+    self.angle_limit_counter, apply_steer_req = common_fault_avoidance(
+      abs(CS.out.steeringAngleDeg) >= MAX_ANGLE, CC.latActive,
+      self.angle_limit_counter, self.maxAngleFrames,
+      MAX_ANGLE_CONSECUTIVE_FRAMES
+    )
+
     lkas_active = CC.latActive
 
-    # Disable steering while turning blinker on and speed below 60 kph
     if CS.out.leftBlinker or CS.out.rightBlinker:
-      self.turning_signal_timer = 0.5 / DT_CTRL  # Disable for 0.5 Seconds after blinker turned off
-    if self.turning_indicator_alert: # set and clear by interface
+      self.turning_signal_timer = 0.5 / DT_CTRL
+    if self.turning_indicator_alert:
       lkas_active = 0
     if self.turning_signal_timer > 0:
       self.turning_signal_timer -= 1
@@ -116,12 +113,12 @@ class CarController:
     if not lkas_active:
       apply_steer = 0
 
-     # Hold torque with induced temporary fault when cutting the actuation bit
     torque_fault = CC.latActive and not apply_steer_req
-    
     self.apply_steer_last = apply_steer
 
-    sys_warning, sys_state, left_lane_warning, right_lane_warning = process_hud_alert(CC.enabled, self.car_fingerprint, hud_control)
+    sys_warning, sys_state, left_lane_warning, right_lane_warning = process_hud_alert(
+      CC.enabled, self.car_fingerprint, hud_control
+    )
 
     if self.haptic_feedback_speed_camera:
       if self.prev_active_cam != self.scc_smoother.active_cam:
@@ -140,47 +137,52 @@ class CarController:
     if clu11_speed > enabled_speed or not lkas_active:
       enabled_speed = clu11_speed
 
-    if self.frame == 0:  # initialize counts from last received count signals
+    if self.frame == 0:
       self.lkas11_cnt = CS.lkas11["CF_Lkas_MsgCount"]
-
     self.lkas11_cnt = (self.lkas11_cnt + 1) % 0x10
 
-    cut_steer_temp = False
-      
+    # ===== Params 주기 로드 =====
     if self.frame % 100 == 0:
       self.maxAngleFrames = int(Params().get("MaxAngleFrames", encoding="utf8"))
-      
+      try:
+        self.jerkStartLimit = float(int(Params().get("JerkStartLimit", encoding="utf8"))) * 0.1
+      except Exception:
+        self.jerkStartLimit = 1.0
+    # ===========================
+
     can_sends = []
-    
-    can_sends.append(create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, apply_steer_req,
-                                   torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled, hud_control.leftLaneVisible, hud_control.rightLaneVisible,
-                                   left_lane_warning, right_lane_warning, 0, self.ldws_opt))
 
-    if CS.mdps_bus or CS.scc_bus == 1:  # send lkas11 bus 1 if mdps or scc is on bus 1
-      can_sends.append(create_lkas11(self.packer, self.frame, self.car_fingerprint, apply_steer, apply_steer_req,
-                                     torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled, hud_control.leftLaneVisible, hud_control.rightLaneVisible,
-                                     left_lane_warning, right_lane_warning, 1, self.ldws_opt))
+    can_sends.append(create_lkas11(
+      self.packer, self.frame, self.car_fingerprint, apply_steer, apply_steer_req,
+      torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
+      hud_control.leftLaneVisible, hud_control.rightLaneVisible,
+      left_lane_warning, right_lane_warning, 0, self.ldws_opt
+    ))
 
-    if self.frame % 2 and CS.mdps_bus: # send clu11 to mdps if it is not on bus 0
+    if CS.mdps_bus or CS.scc_bus == 1:
+      can_sends.append(create_lkas11(
+        self.packer, self.frame, self.car_fingerprint, apply_steer, apply_steer_req,
+        torque_fault, CS.lkas11, sys_warning, sys_state, CC.enabled,
+        hud_control.leftLaneVisible, hud_control.rightLaneVisible,
+        left_lane_warning, right_lane_warning, 1, self.ldws_opt
+      ))
+
+    if self.frame % 2 and CS.mdps_bus:
       can_sends.append(create_clu11(self.packer, CS.mdps_bus, CS.clu11, Buttons.NONE, enabled_speed))
 
-    #if pcm_cancel_cmd and (self.longcontrol and not self.mad_mode_enabled):
-    #  can_sends.append(hyundaican.create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.CANCEL, clu11_speed))
-
-    if CS.mdps_bus or self.car_fingerprint in FEATURES["send_mdps12"]:  # send mdps12 to LKAS to prevent LKAS error
+    if CS.mdps_bus or self.car_fingerprint in FEATURES["send_mdps12"]:
       can_sends.append(create_mdps12(self.packer, self.frame, CS.mdps12))
 
     self.update_auto_resume(CC, CS, clu11_speed, can_sends)
     self.update_scc(CC, CS, actuators, controls, hud_control, can_sends)
 
-    # 20 Hz LFA MFA message
     if self.frame % 5 == 0:
       activated_hda = road_speed_limiter_get_active()
-      # activated_hda: 0 - off, 1 - main road, 2 - highway
       if self.car_fingerprint in FEATURES["send_lfa_mfa"]:
         can_sends.append(create_lfahda_mfc(self.packer, CC.enabled, activated_hda))
       elif CS.has_lfa_hda:
-        can_sends.append(create_hda_mfc(self.packer, activated_hda, CS, hud_control.leftLaneVisible, hud_control.rightLaneVisible))
+        can_sends.append(create_hda_mfc(self.packer, activated_hda, CS,
+                                        hud_control.leftLaneVisible, hud_control.rightLaneVisible))
 
     new_actuators = actuators.copy()
     new_actuators.steer = apply_steer / self.params.STEER_MAX
@@ -190,7 +192,6 @@ class CarController:
     return new_actuators, can_sends
 
   def update_auto_resume(self, CC, CS, clu11_speed, can_sends):
-    # fix auto resume - by neokii
     if CC.cruiseControl.resume and not CS.out.gasPressed:
       if self.last_lead_distance == 0:
         self.last_lead_distance = CS.lead_distance
@@ -206,43 +207,35 @@ class CarController:
       elif abs(CS.lead_distance - self.last_lead_distance) > 0.1:
         can_sends.append(create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed))
         self.resume_cnt += 1
-
         if self.resume_cnt >= int(randint(4, 5) * 2):
           self.resume_cnt = 0
           self.resume_wait_timer = int(randint(20, 25) * 2)
- 
 
     elif self.last_lead_distance != 0:
       self.last_lead_distance = 0
 
   def update_scc(self, CC, CS, actuators, controls, hud_control, can_sends):
-
-    # scc smoother
     self.scc_smoother.update(CC.enabled, can_sends, self.packer, CC, CS, self.frame, controls)
 
-    # send scc to car if longcontrol enabled and SCC not on bus 0 or ont live
     if self.longcontrol and CS.cruiseState_enabled and (CS.scc_bus or not self.scc_live):
-
       if self.frame % 2 == 0:
-
         set_speed = hud_control.setSpeed
         if not (min_set_speed < set_speed < 255 * CV.KPH_TO_MS):
           set_speed = min_set_speed
         set_speed *= CV.MS_TO_MPH if CS.is_set_speed_in_mph else CV.MS_TO_KPH
 
         stopping = controls.LoC.long_control_state == LongCtrlState.stopping
-        apply_accel = clip(actuators.accel if CC.longActive else 0, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
+
+        apply_accel = clip(actuators.accel if CC.longActive else 0,
+                           CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
         apply_accel = self.scc_smoother.get_apply_accel(CS, controls.sm, apply_accel, stopping)
-
         self.accel = apply_accel
-
         controls.apply_accel = apply_accel
+
         aReqValue = CS.scc12["aReqValue"]
         controls.aReqValue = aReqValue
-
         if aReqValue < controls.aReqValueMin:
           controls.aReqValueMin = controls.aReqValue
-
         if aReqValue > controls.aReqValueMax:
           controls.aReqValueMax = controls.aReqValue
 
@@ -257,44 +250,79 @@ class CarController:
 
         if self.scc12_cnt < 0:
           self.scc12_cnt = CS.scc12["CR_VSM_Alive"] if not CS.no_radar else 0
+        self.scc12_cnt = (self.scc12_cnt + 1) % 0xF
 
-        self.scc12_cnt += 1
-        self.scc12_cnt %= 0xF
+        can_sends.append(create_scc12(
+          self.packer, apply_accel, CC.enabled, self.scc12_cnt, self.scc_live, CS.scc12,
+          CC.cruiseControl.override, CS.out.brakePressed, CC.cruiseControl.resume,
+          self.car_fingerprint
+        ))
 
-        can_sends.append(create_scc12(self.packer, apply_accel, CC.enabled, self.scc12_cnt, self.scc_live, CS.scc12,
-                                      CC.cruiseControl.override, CS.out.brakePressed, CC.cruiseControl.resume,
-                                      self.car_fingerprint))
-
-        can_sends.append(create_scc11(self.packer, self.frame, CC.enabled, set_speed, hud_control.leadVisible, self.scc_live, CS.scc11,
-                       self.scc_smoother.active_cam, stock_cam))
+        can_sends.append(create_scc11(
+          self.packer, self.frame, CC.enabled, set_speed, hud_control.leadVisible,
+          self.scc_live, CS.scc11, self.scc_smoother.active_cam, stock_cam
+        ))
 
         if self.frame % 20 == 0 and CS.has_scc13:
           can_sends.append(create_scc13(self.packer, CS.scc13))
 
         if CS.has_scc14:
           acc_standstill = stopping if CS.out.vEgo < 2. else False
-          #stopping = stopping and CS.out.vEgoRaw < 0.05
-          
-          required_jerk = min(3, abs(accel - CS.out.aEgo) * 50)
-          lower_jerk = required_jerk
-          upper_jerk = required_jerk
 
-          if CS.out.aEgo < accel:
-            lower_jerk = 0
+          # ===== apilot jerk (JerkStartLimit 통합, softHold 없음) =====
+          jerk = getattr(actuators, "jerk", 0.0)
+          startingJerk = self.jerkStartLimit
+          jerkLimit = 5.0
+
+          self.jerk_count += DT_CTRL
+          jerk_max = interp(self.jerk_count, [0, 1.5, 2.5], [startingJerk, startingJerk, jerkLimit])
+
+          long_state = controls.LoC.long_control_state
+          if long_state == LongCtrlState.off:
+            upper_jerk = jerkLimit
+            lower_jerk = jerkLimit
+            self.jerk_count = 0.0
+          elif long_state == LongCtrlState.stopping:
+            upper_jerk = 0.5
+            lower_jerk = jerkLimit
+            self.jerk_count = 0.0
           else:
-            upper_jerk = 0
+            upper_jerk = min(max(0.5, jerk * 2.0), jerk_max)
+            lower_jerk = min(max(1.0, -jerk * 2.0), jerk_max)
+          # ===========================================================
 
+          # ===== ajouatom style ComfortBand (accel 기반) =====
+          cb_upper = clip(0.9 + apply_accel * 0.2, 0.0, 1.2)
+          cb_lower = clip(0.8 + apply_accel * 0.2, 0.0, 1.2)
+          # =================================================
+
+          # ===== ajouatom style ObjGap / ObjGap2 =====
           lead = self.scc_smoother.get_lead(controls.sm)
-
           if lead is not None:
-            d = lead.dRel
-            obj_gap = 1 if d < 25 else 2 if d < 40 else 3 if d < 60 else 4 if d < 80 else 5
+            d = float(lead.dRel)
+            vrel = float(getattr(lead, "vRel", 0.0))
+            obj_gap = 2 if d < 25 else 3 if d < 40 else 4 if d < 70 else 5
+            obj_gap2 = 2 if vrel < -0.2 else 1
           else:
             obj_gap = 0
+            obj_gap2 = 0
+          # ==========================================
 
-          can_sends.append(
-            create_scc14(self.packer, CC.enabled, CS.out.vEgo, acc_standstill, apply_accel, upper_jerk, lower_jerk, stopping, CC.cruiseControl.override,
-                         obj_gap, CS.scc14))
-          self.apply_accel_last = apply_accel
+          can_sends.append(create_scc14(
+            self.packer,
+            CC.enabled,
+            CS.out.vEgo,
+            acc_standstill,
+            apply_accel,
+            upper_jerk,
+            lower_jerk,
+            cb_upper,
+            cb_lower,
+            CC.cruiseControl.override,   # long_override
+            obj_gap,
+            obj_gap2,
+            CS.scc14
+          ))
+
     else:
       self.scc12_cnt = -1
