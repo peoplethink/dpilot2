09f4c5719cfa8bfff70a1761ff42af488af1cba1
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-20 06:05:31 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -7,7 +7,7 @@ from selfdrive.car import apply_std_steer_torque_limits, common_fault_avoidance
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, \
   create_scc11, create_scc12, create_scc13, create_scc14, \
   create_mdps12, create_lfahda_mfc, create_hda_mfc
-#from selfdrive.car.hyundai.scc_smoother import SccSmoother
+from selfdrive.car.hyundai.scc_smoother import SccSmoother
 from selfdrive.car.hyundai.values import Buttons, CAR, FEATURES, CarControllerParams, LEGACY_SAFETY_MODE_CAR
 from opendbc.can.packer import CANPacker
 from common.conversions import Conversions as CV
