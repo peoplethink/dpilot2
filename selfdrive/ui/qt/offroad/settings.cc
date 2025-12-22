747e1b3d4de4b8573eb66183337999df3fae0e7c
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-22 18:49:27 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -39,7 +39,7 @@
 
 TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
   // param, title, desc, icon
-  std::vector<std::tuple<QString, QString, QString, QString>> toggles{
+  std::vector<std::tuple<QString, QString, QString, QString>> toggle_defs{
     {
       "OpenpilotEnabledToggle",
       "오픈파일럿 사용",
@@ -94,7 +94,7 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
 
   };
 
-  for (auto &[param, title, desc, icon] : toggle_defs{
+  for (auto &[param, title, desc, icon] : toggle_defs) {
     auto toggle = new ParamControl(param, title, desc, icon, this);
   
     bool locked = params.getBool((param + "Lock").toStdString());
