c2f0ec63f2406bdfb1c2cb50859b776cf3bf9950
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-22 20:40:49 +0900

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -249,7 +249,7 @@ class CarInterfaceBase(ABC):
   def get_params(cls, candidate: str, fingerprint: Dict[int, Dict[int, int]], car_fw: List[car.CarParams.CarFw], experimental_long: bool = False):
 
     ret = CarInterfaceBase.get_std_params(candidate)
-    ret = cls._get_params(ret, candidate, fingerprint, car_fw, disable_radar)
+    ret = cls._get_params(ret, candidate, fingerprint, car_fw, experimental_long)
 
     # Set common params using fields set by the car interface
     # TODO: get actual value, for now starting with reasonable value for
