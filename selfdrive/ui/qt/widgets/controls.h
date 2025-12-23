ae7f53fa2c623b9ac658a112ad0af115977c83dc
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-23 15:34:34 +0900

    Update controls.h

--- selfdrive/ui/qt/widgets/controls.h
+++ selfdrive/ui/qt/widgets/controls.h
@@ -137,14 +137,9 @@ public:
       if (!confirm || confirmed || !state || dialog.exec()) {
         if (store_confirm && state) params.putBool(key + "Confirmed", true);
         params.putBool(key, state);
+        setIcon(state);
       } else {
         toggle.togglePosition();
-      }
-      
-      if (state && !active_icon_pixmap.isNull()) {
-        icon_label->setPixmap(active_icon_pixmap);
-      } else if (!icon_pixmap.isNull()) {
-        icon_label->setPixmap(icon_pixmap);
       }
     });
   }
@@ -159,8 +154,10 @@ public:
   }
 
   void refresh() {
-    if (params.getBool(key) != toggle.on) {
+    bool state = params.getBool(key);
+    if (state != toggle.on) {
       toggle.togglePosition();
+      setIcon(state);
     }
   };
 
@@ -169,6 +166,14 @@ public:
   };
 
 private:
+  void setIcon(bool state) {
+    if (state && !active_icon_pixmap.isNull()) {
+      icon_label->setPixmap(active_icon_pixmap);
+    } else if (!icon_pixmap.isNull()) {
+      icon_label->setPixmap(icon_pixmap);
+    }
+  };
+
   std::string key;
   Params params;
   QPixmap active_icon_pixmap;
