92c4706beb3a44cd0c1aade248e8506b24e1dfbb
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-23 21:46:27 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -281,6 +281,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CruiseMaxVals4", PERSISTENT},
     {"CruiseMaxVals5", PERSISTENT},
     {"CruiseMaxVals6", PERSISTENT},
+    {"SccConnectedBus2", PERSISTENT},
     {"NNFF", PERSISTENT},
     {"NNFFLite", PERSISTENT},
     {"NNFFModelName", PERSISTENT},
