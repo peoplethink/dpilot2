2a61640e3c14f188081f5afa22955abbc41b454e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-23 15:31:10 +0900

    Update controls.h

--- selfdrive/ui/qt/widgets/controls.h
+++ selfdrive/ui/qt/widgets/controls.h
@@ -46,6 +46,9 @@ public:
     return description->text();
   }
 
+  QLabel *icon_label;
+  QPixmap icon_pixmap;
+
 signals:
   void showDescription();
 
@@ -123,7 +126,7 @@ class ParamControl : public ToggleControl {
   Q_OBJECT
 
 public:
-  ParamControl(const QString &param, const QString &title, const QString &desc, const QString &icon, const bool _confirm, QWidget *parent = nullptr) : confirm(_confirm), ToggleControl(title, desc, icon, false, parent) {
+  ParamControl(const QString &param, const QString &title, const QString &desc, const QString &icon, QWidget *parent = nullptr) : ToggleControl(title, desc, icon, false, parent) {
     key = param.toStdString();
     QObject::connect(this, &ParamControl::toggleFlipped, [=](bool state) {
       QString content("<body><h2>" + title + "</h2><br><br>"
@@ -137,11 +140,23 @@ public:
       } else {
         toggle.togglePosition();
       }
+      
+      if (state && !active_icon_pixmap.isNull()) {
+        icon_label->setPixmap(active_icon_pixmap);
+      } else if (!icon_pixmap.isNull()) {
+        icon_label->setPixmap(icon_pixmap);
+      }
     });
   }
 
-  bool confirm = false;
-  bool store_confirm = false;
+  void setConfirmation(bool _confirm, bool _store_confirm) {
+    confirm = _confirm;
+    store_confirm = _store_confirm;
+  };
+
+  void setActiveIcon(const QString &icon) {
+    active_icon_pixmap = QPixmap(icon).scaledToWidth(80, Qt::SmoothTransformation);
+  }
 
   void refresh() {
     if (params.getBool(key) != toggle.on) {
@@ -156,6 +171,9 @@ public:
 private:
   std::string key;
   Params params;
+  QPixmap active_icon_pixmap;
+  bool confirm = false;
+  bool store_confirm = false;
 };
 
 class ListWidget : public QWidget {
