8287f98f961cb524149368a65f3a1235093ecb93
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-30 12:56:16 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -552,7 +552,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
       Priority.LOW, VisualAlert.none, AudibleAlert.none, 2.),
   },
 
-  EventName.visionLeaving: {
+  EventName.visionleaving: {
     ET.WARNING: Alert(
       "Curve Leaving",
       "",
