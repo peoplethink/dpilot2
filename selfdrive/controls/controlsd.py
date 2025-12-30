7729d3b4a8db5be8e49fb1afb2a810803d33da2f
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2025-12-30 13:07:52 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -23,7 +23,7 @@ from selfdrive.controls.lib.latcontrol_indi import LatControlINDI
 from selfdrive.controls.lib.latcontrol_lqr import LatControlLQR
 from selfdrive.controls.lib.latcontrol_angle import LatControlAngle
 from selfdrive.controls.lib.latcontrol_torque import LatControlTorque
-from selfdrive.controls.lib.events import Events, ET
+from selfdrive.controls.lib.events import Events, ET, EVENTS
 from selfdrive.controls.lib.alertmanager import AlertManager, set_offroad_alert
 from selfdrive.controls.lib.vehicle_model import VehicleModel
 from selfdrive.hardware import HARDWARE, TICI, EON
