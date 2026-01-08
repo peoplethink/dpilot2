0962befe263adabc797faac95e86cecad80ac61b
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-01-08 09:20:44 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -27,13 +27,6 @@ class CarInterface(CarInterfaceBase):
     self.mad_mode_enabled = Params().get_bool('MadModeEnabled')
 
   @staticmethod
-  def get_pid_accel_limits(CP, current_speed, cruise_speed):
-    v_current_kph = current_speed * CV.MS_TO_KPH
-    gas_max_bp = [10., 20., 50., 70., 130., 150.]
-    gas_max_v = [1.45, 1.15, 0.5, 0.32, 0.17, 0.1]
-    return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
-
-  @staticmethod
   def _get_params(ret, candidate, fingerprint, car_fw, experimental_long=False):
     params = Params()
 
